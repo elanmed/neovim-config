51f81d547f0a1e129af7b9e512bd6fe45cc6a53f
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-09-27 22:07:48 -0400

    update barebones options

--- lua/barebones/options.lua
+++ lua/barebones/options.lua
@@ -1,5 +1,17 @@
 vim.cmd("colorscheme slate")
-vim.cmd([[
-  let g:netrw_winsize = 30
-  let g:netrw_banner = 0
-]])
+vim.g.netrw_winsize = 40
+vim.g.netrw_banner = 0 -- removes banner at the top
+
+vim.api.nvim_create_autocmd('filetype', {
+  pattern = 'netrw',
+  callback = function()
+    local nmap = function(lhs, rhs)
+      vim.keymap.set('n', lhs, rhs, { remap = true, buffer = true })
+    end
+
+    nmap("h", "-^") -- go up a directory
+    nmap("l", "<cr>")
+    nmap("P", "<C-w>z") -- close preview
+    nmap("<leader>rb", ":Lexplore<cr>") -- close netrw
+  end
+})
