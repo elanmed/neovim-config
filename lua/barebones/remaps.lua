11dd226ae2b83b5f2b922ac49e87ba5a67b5fa20
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-09-27 22:08:23 -0400

    typo

--- lua/barebones/remaps.lua
+++ lua/barebones/remaps.lua
@@ -1,4 +1,4 @@
 package.path = package.path .. ";" .. os.getenv("HOME") .. "/.config/nvim/?.lua"
 local h = require("shared.helpers")
 
-h.nmap("<leader>rb", ":Vexplore<cr>")
+h.nmap("<leader>rb", ":Lexplore<cr>")
