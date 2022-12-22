fbafc227f14aeb0be68e927afd9777b82ef34f90
Elan <54599408+ElanMedoff@users.noreply.github.com>
2022-12-21 22:41:02 -0500

    update treesitter ensure installed

--- lua/settings/plugins/treesitter.lua
+++ lua/settings/plugins/treesitter.lua
@@ -4,10 +4,11 @@ if not ok then
 end
 
 configs.setup({
-  ensure_installed = "all",
+  ensure_installed = { "bash", "comment", "css", "html", "javascript", "json", "json5", "jsonc", "lua", "markdown",
+    "regex", "ruby", "scss", "tsx", "typescript", "yaml", },
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = { 'org' }, -- prevent vim highlight from interfering with treesitter
+    additional_vim_regex_highlighting = false, -- prevent vim highlight from interfering with treesitter
   },
   indent = { enable = true },
   rainbow = {
