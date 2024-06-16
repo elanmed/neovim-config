200efecf2dba6c99dcb1f81dc8dc2568f8159b38
Elan Medoff <elanmedoff@Elans-MacBook-Pro.local>
2024-06-15 22:52:47 -0400

    update bootstrap script to support dnf

--- bootstrap.ts
+++ bootstrap.ts
@@ -1,13 +1,6 @@
 import "npm:zx/globals";
-import { $ } from "npm:zx";
-import { hasHomebrew, maybeInstallPackage } from "../../../helpers.ts";
+import { installPackage } from "../../../helpers.ts";
 
-const hasBrew = await hasHomebrew();
-if (!hasBrew) {
-  await $`exit`;
-}
-
-await maybeInstallPackage("neovim");
-await maybeInstallPackage("ripgrep");
-await maybeInstallPackage("fzf");
-await maybeInstallPackage("watchman");
+const packageManager = argv.pm;
+await installPackage({ packageName: "fzf", packageManager });
+await installPackage({ packageName: "watchman", packageManager });
