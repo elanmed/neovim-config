41b544421b6c19c5b617d566764e07244566a155
Elan Medoff <elanmedoff@Elans-MacBook-Pro.local>
2024-07-07 13:04:43 -0400

    unbind c-p in neoclip

--- lua/settings/plugins/neoclip.lua
+++ lua/settings/plugins/neoclip.lua
@@ -4,6 +4,13 @@ local telescope = require "telescope"
 
 neoclip.setup({
   history = 25,
+  keys = {
+    telescope = {
+      i = {
+        paste = "<f1>", -- unbind <C-p>, but this doesn't accept nil or ""
+      },
+    },
+  },
 })
 h.nmap("<leader>yo", telescope.extensions.neoclip.default)
 h.vmap("<leader>yo", telescope.extensions.neoclip.default)
