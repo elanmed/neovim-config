5d27835911fafe4601add0a165f3f80029284b45
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-07-21 00:01:46 -0400

    convert deno scripts to bash

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+source ~/.dotfiles/helpers.sh
+
+h_validate_num_args --num=1 "$@"
+h_validate_package_manager $1
+
+h_install_package $1 neovim
+if [[ $1 == "--pm=dnf" ]]
+then
+  h_install_package $1 python3-neovim
+fi
+
+h_install_package $1 fzf
+h_install_package $1 ripgrep
+h_install_package $1 watchman
+
+# increase num allowed open fd
+ulimit -n 1024
+h_cecho --doing "opening nvim"
+nvim ~/.config/nvim/lua/settings/plugins/packer.lua -c "w"
