efee6612538238e668e1bd0b3674fe5db6a81edf
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-08-18 22:11:41 -0400

    remove print

--- lua/settings/plugins/mini_map.lua
+++ lua/settings/plugins/mini_map.lua
@@ -15,7 +15,6 @@ local hide_mini = false
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = "*",
   callback = function()
-    print('Bufenter', hide_mini)
     if h.table_contains({ "oil" }, vim.bo.filetype) or hide_mini then
       require("mini.map").close()
     else
@@ -27,7 +26,6 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 vim.api.nvim_create_autocmd({ "TabEnter" }, {
   pattern = "*",
   callback = function()
-    print('TabEnter')
     if vim.fn.winnr('$') > 2 then
       hide_mini = true
       require("mini.map").close()
