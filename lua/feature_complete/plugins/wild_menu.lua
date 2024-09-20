31d31f93707942327ab3187419c5946b35aa3f54
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-09-20 00:48:26 -0400

    remove rounded border, remove fuzzy file finder

--- lua/feature_complete/plugins/wild_menu.lua
+++ lua/feature_complete/plugins/wild_menu.lua
@@ -6,15 +6,15 @@ h.cmap("<S-Tab>", "")
 local wilder = require('wilder')
 
 wilder.setup({ modes = { ':' }, next_key = "<C-n>", previous_key = "<C-p>" })
--- wilder.set_option('use_python_remote_plugin', 0)
+wilder.set_option('use_python_remote_plugin', 0)
 
 wilder.set_option('pipeline', {
   wilder.branch(
-    wilder.python_file_finder_pipeline({
-      file_command = { 'rg', '--files' },
-      dir_command = { 'fd', '-tf' },
-      filters = { 'fuzzy_filter', 'difflib_sorter' },
-    }),
+  -- wilder.python_file_finder_pipeline({
+  --   file_command = { 'rg', '--files' },
+  --   dir_command = { 'fd', '-tf' },
+  --   filters = { 'fuzzy_filter', 'difflib_sorter' },
+  -- }),
     wilder.cmdline_pipeline({
       fuzzy = 1,
       fuzzy_filter = wilder.lua_fzy_filter(),
@@ -24,7 +24,6 @@ wilder.set_option('pipeline', {
 
 wilder.set_option('renderer', wilder.popupmenu_renderer(
   wilder.popupmenu_border_theme({
-    border = 'rounded',
     empty_message = wilder.popupmenu_empty_message_with_spinner(),
     highlighter = wilder.lua_fzy_highlighter(),
     highlights = {
