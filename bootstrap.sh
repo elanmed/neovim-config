a734f8fc2ba1143a495fdae4072fc00411ccb6a8
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-05 21:44:58 -0400

    bootstrap cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,20 +3,20 @@
 source ~/.dotfiles/helpers.sh
 
 h_validate_num_args --num=1 "$@"
-h_validate_package_manager $1
+h_validate_package_manager "$1"
 
-h_install_package $1 neovim
+h_install_package "$1" "neovim"
 if [[ $1 == "--pm=dnf" ]]
 then
-  h_install_package $1 python3-neovim
+  h_install_package "$1" "python3-neovim"
 fi
 
-h_install_package $1 fzf
-h_install_package $1 ripgrep
-h_install_package $1 watchman
-h_install_package $1 fd-find
+h_install_package "$1" "fzf"
+h_install_package "$1" "ripgrep"
+h_install_package "$1" "watchman"
+h_install_package "$1" "fd-find"
 
 # increase num allowed open fd
 ulimit -n 1024
-h_cecho --doing "running :PlugInstall"
+h_echo --mode=doing "running :PlugInstall"
 nvim --headless "+PlugInstall" +qa
