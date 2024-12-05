a1f6ff3ff45446722a3816beacb1dd5f5e9e7231
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-12-04 23:19:13 -0500

    move closer to wild menu example config

--- lua/feature_complete/plugins/wild_menu.lua
+++ lua/feature_complete/plugins/wild_menu.lua
@@ -1,7 +1,7 @@
 local colors = require "feature_complete.plugins.colorscheme"
-
 local wilder = require "wilder"
 
+-- https://github.com/gelguy/wilder.nvim?tab=readme-ov-file#neovim-lua-only-config
 wilder.setup { modes = { ":", }, next_key = "<C-n>", previous_key = "<C-p>", }
 wilder.set_option("use_python_remote_plugin", 0)
 wilder.set_option("pipeline", {
@@ -9,13 +9,14 @@ wilder.set_option("pipeline", {
     wilder.cmdline_pipeline {
       fuzzy = 1,
       fuzzy_filter = wilder.lua_fzy_filter(),
-    }
+    },
+    wilder.vim_search_pipeline()
+
   ),
 })
 wilder.set_option("renderer", wilder.popupmenu_renderer(
   wilder.popupmenu_border_theme {
     border = { "", "", "", "", "", "", "", "", },
-    empty_message = wilder.popupmenu_empty_message_with_spinner(),
     highlighter = wilder.lua_fzy_highlighter(),
     highlights = {
       accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1, }, { a = 1, }, { foreground = colors.orange, }, }),
