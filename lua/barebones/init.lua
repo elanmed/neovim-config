bbea56ff9a514caa7183bf0c0a7954de61402f26
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-12-24 19:40:42 -0500

    add approximate alternatives in the barebones configs for common remaps

--- lua/barebones/init.lua
+++ lua/barebones/init.lua
@@ -9,6 +9,15 @@ vim.api.nvim_create_autocmd({ "FileType", }, {
   end,
 })
 
+vim.api.nvim_create_autocmd({ "FileType", }, {
+  pattern = "wildmenu",
+  callback = function()
+    h.nmap("<C-n>", "<tab>")
+    h.nmap("<C-p>", "<S-tab>")
+  end,
+})
+
+h.nmap("z", "zz")
 h.nmap("L", h.user_cmd_cb "bnext", { desc = "Next buffer", })
 h.nmap("H", h.user_cmd_cb "bprev", { desc = "Previous buffer", })
 h.nmap("<C-f>", function()
@@ -18,3 +27,12 @@ h.nmap("<C-f>", function()
     vim.cmd "Explore %:p:h"
   end
 end, { desc = "Toggle netrw, focusing the current file", })
+vim.opt.path:append "**" -- search in subfolder
+vim.cmd "nnoremap <C-p> :find<space>"
+h.imap("<C-s>", "<C-n>") -- autocomplete
+vim.cmd "nnoremap <C-g> :buffer<space>"
+vim.cmd "nnoremap <leader>lg :grep<space>"
+
+h.nmap("gd", "<nop>")
+h.nmap("gh", "<nop>")
+h.nmap("<C-b>", "<nop>")
