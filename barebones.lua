9411426895b4012b3a0ed9ca2a69387a0fa3a663
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-12-25 23:37:47 -0500

    add colorscheme for barebones config

--- barebones.lua
+++ barebones.lua
@@ -4,6 +4,7 @@ local h = require "shared.helpers"
 h.map("", "<space>", "<nop>")
 h.let.mapleader = " "
 h.let.maplocalleader = " "
+vim.cmd "colorscheme desert"
 
 require "shared.options"
 require "shared.remaps"
