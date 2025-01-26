473eed05f9b5adf29946ca6857df68c44ebab97c
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-01-25 22:59:58 -0500

    update wildchar to match wilder

--- lua/shared/options.lua
+++ lua/shared/options.lua
@@ -1,40 +1,41 @@
-local h = require "shared.helpers"
+local h              = require "shared.helpers"
 
 -- h.set.colorcolumn = "120"
-h.set.clipboard = "unnamedplus" -- os clipboard
-h.set.number = true             -- line numbers
-h.set.errorbells = false        -- disable error beep
-h.set.mouse = "nvi"             -- send vim commands with mouse in vim, prevents conflicts with tmux
-h.set.confirm = true            -- prompt to save before quitting
-h.set.linebreak = true          -- won't break on word when wrapping
-h.set.splitright = true         -- when splitting vertically, open new split to the right
-h.set.splitbelow = true         -- when splitting horizontally, open new split below
-h.set.relativenumber = true     -- useful for multiline j, k
-h.set.termguicolors = true      -- needed for modern themes
-h.set.scrolloff = 8
-h.set.fileencoding = "utf-8"
-h.set.conceallevel = 0 -- keep quotes keys in normal mode
+h.set.clipboard      = "unnamedplus" -- os clipboard
+h.set.number         = true          -- line numbers
+h.set.errorbells     = false         -- disable error beep
+h.set.mouse          = "nvi"         -- send vim commands with mouse in vim, prevents conflicts with tmux
+h.set.confirm        = true          -- prompt to save before quitting
+h.set.linebreak      = true          -- won't break on word when wrapping
+h.set.splitright     = true          -- when splitting vertically, open new split to the right
+h.set.splitbelow     = true          -- when splitting horizontally, open new split below
+h.set.relativenumber = true          -- useful for multiline j, k
+h.set.termguicolors  = true          -- needed for modern themes
+h.set.scrolloff      = 8
+h.set.fileencoding   = "utf-8"
+h.set.conceallevel   = 0     -- keep quotes keys in normal mode
+vim.cmd "set wildchar=<C-n>" -- TODO: issues setting in lua
 
 -- ignore checkhealth error
 h.let.loaded_perl_provider = 0
 
 -- spell
-h.set.spelllang = "en_us"
-h.set.spell = false -- TODO: look into
+h.set.spelllang            = "en_us"
+h.set.spell                = false -- TODO: look into
 
 -- disable vim backups
-h.set.swapfile = false
-h.set.backup = false
-h.set.writebackup = false
+h.set.swapfile             = false
+h.set.backup               = false
+h.set.writebackup          = false
 
 -- tabs
-h.set.expandtab = true -- use spaces in tabs
-h.set.tabstop = 2      -- number of columns in a tab
-h.set.softtabstop = 2  -- number of spaces to delete when deleting a tab
-h.set.shiftwidth = 2   -- number of spaces to insert/delete when in insert mode
+h.set.expandtab            = true -- use spaces in tabs
+h.set.tabstop              = 2    -- number of columns in a tab
+h.set.softtabstop          = 2    -- number of spaces to delete when deleting a tab
+h.set.shiftwidth           = 2    -- number of spaces to insert/delete when in insert mode
 
 -- search
-h.set.ignorecase = true
+h.set.ignorecase           = true
 h.keys.map({ "n", }, "<leader>/t", h.keys.user_cmd_cb "noh", { desc = "Turn off highlighting", })
 
 -- folding
