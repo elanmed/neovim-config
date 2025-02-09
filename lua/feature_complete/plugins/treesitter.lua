779e4ed57103101468c8c0dff2106162c12ee4bd
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-02-09 14:57:17 -0500

    cut down on treesitter text objects

--- lua/feature_complete/plugins/treesitter.lua
+++ lua/feature_complete/plugins/treesitter.lua
@@ -33,18 +33,7 @@ require "nvim-treesitter.configs".setup {
     select = {
       enable = true,
       lookahead = true,
-      -- TODO: use these more
       keymaps = {
-        ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment", },
-        ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment", },
-        ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment", },
-        ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment", },
-
-        ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property", },
-        ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property", },
-        ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property", },
-        ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property", },
-
         ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional", },
         ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional", },
 
