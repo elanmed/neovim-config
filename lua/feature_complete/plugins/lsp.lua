c00417d6c610cf545e18699703bf686715324ecf
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-02-11 22:58:34 -0500

    add prettier filetypes to conform setup

--- lua/feature_complete/plugins/lsp.lua
+++ lua/feature_complete/plugins/lsp.lua
@@ -169,6 +169,20 @@ cmp.setup {
 require "lazydev".setup {}
 require "nvim-autopairs".setup {}
 require "conform".setup {
+  formatters_by_ft = {
+    css = { "prettier", },
+    graphql = { "prettier", },
+    html = { "prettier", },
+    javascript = { "prettier", },
+    javascriptreact = { "prettier", },
+    json = { "prettier", },
+    less = { "prettier", },
+    markdown = { "prettier", },
+    scss = { "prettier", },
+    typescript = { "prettier", },
+    typescriptreact = { "prettier", },
+    yaml = { "prettier", },
+  },
   format_after_save = {
     lsp_format = "fallback",
     async = true,
