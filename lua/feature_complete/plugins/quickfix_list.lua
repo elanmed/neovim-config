722546d8a5adb88e85d868a588b9e238299b5306
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-02-13 21:47:19 -0500

    try out quicker

--- lua/feature_complete/plugins/quickfix_list.lua
+++ lua/feature_complete/plugins/quickfix_list.lua
@@ -1,21 +1,42 @@
 local h = require "shared.helpers"
-local bqf = require "bqf"
+-- local bqf = require "bqf"
 
-bqf.setup {
-  auto_resize_height = true,
-  func_map = {
-    openc = "<cr>",
-    open = "o",
-  },
-  preview = {
-    winblend = 0,
+-- bqf.setup {
+--   auto_resize_height = true,
+--   func_map = {
+--     openc = "<cr>",
+--     open = "o",
+--   },
+--   preview = {
+--     winblend = 0,
+--   },
+-- }
+
+local quicker = require "quicker"
+
+quicker.setup {
+  keys = {
+    {
+      ">",
+      function()
+        quicker.expand { before = 2, after = 2, add_to_existing = true, }
+      end,
+      desc = "Expand quickfix context",
+    },
+    {
+      "<",
+      function()
+        quicker.collapse()
+      end,
+      desc = "Collapse quickfix context",
+    },
   },
 }
 
 vim.api.nvim_create_autocmd({ "BufEnter", }, {
   pattern = "*",
   callback = function()
-    if h.tbl.table_contains_value({ "qf", "aerial", "undotree", }, vim.bo.filetype) then
+    if h.tbl.table_contains_value({ "aerial", "undotree", }, vim.bo.filetype) then
       h.set.cursorline = true
     else
       h.set.cursorline = false
