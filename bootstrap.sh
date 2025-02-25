105180b44337949404f22522219e31e99bec3c84
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-02-24 23:56:55 -0500

    remove snacks image, deps in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,12 +37,6 @@ else
   h_install_package "$package_manager" fd
 fi
 
-if h_is_linux; then
-  h_install_package "$package_manager" ImageMagick
-else
-  h_install_package "$package_manager" imagemagick
-fi
-
 if $server_flag; then
   h_echo --mode=noop "SKIPPING: running :PlugInstall"
 else
--- lua/feature_complete/plugins/misc.lua
+++ lua/feature_complete/plugins/misc.lua
@@ -2,7 +2,6 @@ local h = require "shared.helpers"
 
 --- @type snacks.Config
 require "snacks".setup {
-  image = { enabled = true, },
   indent = { enabled = true, animate = { enabled = false, }, },
   explorer = { enabled = true, replace_netrw = false, },
 }
