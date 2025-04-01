8ed9b4e08f83b32c5f2b3fb87d1715689daa5a40
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-03-31 23:04:50 -0400

    remove lsp progress

--- lua/feature_complete/plugins/lsp.lua
+++ lua/feature_complete/plugins/lsp.lua
@@ -92,22 +92,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
 })
 
--- https://github.com/folke/snacks.nvim/blob/main/docs/notifier.md#-examples
-vim.api.nvim_create_autocmd("LspProgress", {
-  --- @param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-  callback = function(ev)
-    local spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏", }
-    vim.notify(vim.lsp.status(), "info", {
-      id = "lsp_progress",
-      title = "LSP Progress",
-      opts = function(notif)
-        notif.icon = ev.data.params.value.kind == "end" and " "
-            or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-      end,
-    })
-  end,
-})
-
 h.keys.map({ "n", }, "gh", function() vim.lsp.buf.hover { border = "single", } end, { desc = "LSP hover", })
 h.keys.map({ "n", }, "gd", vim.lsp.buf.definition, { desc = "LSP go to definition", })
 h.keys.map({ "n", }, "gs", vim.lsp.buf.type_definition, { desc = "LSP go to type definition", })
