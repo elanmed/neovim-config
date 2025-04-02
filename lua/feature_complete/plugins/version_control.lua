d5e642094dd30e027433a24150a377915b8be924
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-04-02 12:01:12 -0400

    cleanup git remaps

--- lua/feature_complete/plugins/version_control.lua
+++ lua/feature_complete/plugins/version_control.lua
@@ -33,12 +33,11 @@ diffview.setup {
     },
     file_panel = {
       { "n", "X", actions.restore_entry, { desc = "Restore entry to the state on the left side", }, },
-      { "n", ")", actions.next_conflict, { desc = "In the merge-tool: jump to the next conflict", }, },
-      { "n", "(", actions.prev_conflict, { desc = "In the merge-tool: jump to the previous conflict", }, },
       { "n", "<leader>go", actions.conflict_choose_all "ours", { desc = "Use the OURS version of a conflict for the whole file", }, },
       { "n", "<leader>gt", actions.conflict_choose_all "theirs", { desc = "Use the THEIRS version of a conflict for the whole file", }, },
       { "n", "<leader>ga", actions.conflict_choose_all "all", { desc = "Use all the versions of a conflict for the whole file", }, },
       { "n", "-", actions.toggle_stage_entry, { desc = "Stage / unstage the selected entry", }, },
+      { "n", "o", actions.goto_file_edit, { desc = "Open the file in the previous tabpage", }, },
 
       { "n", "<cr>", actions.select_entry, { desc = "Open the diff for the selected entry", }, },
       { "n", "<C-n>", actions.select_next_entry, { desc = "Open the diff for the next file", }, },
@@ -102,8 +101,6 @@ end, { desc = "Reset the current bUffer", })
 h.keys.map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview the current hunk", })
 h.keys.map("n", "<leader>hl", gitsigns.toggle_current_line_blame,
   { desc = "Toggle git blame for the current line", })
-
--- fugitive
 h.keys.map("n", "<leader>hd", function()
     local current_buf = vim.api.nvim_get_current_buf()
     vim.cmd "tabnew"
@@ -112,9 +109,9 @@ h.keys.map("n", "<leader>hd", function()
   end,
   { desc = "Open the Diff for the current file", })
 h.keys.map("n", "<leader>hq", function() h.notify.error "use <C-y> instead!" end)
+
 h.keys.map("n", "<leader>gs", h.keys.vim_cmd_cb "Gedit :",
   { desc = "Open the fugitive status in the current tab", })
-h.keys.map("n", "<leader>gp", h.keys.vim_cmd_cb "Git push origin HEAD", { desc = "Git Push origin HEAD", })
 h.keys.map("n", "<leader>gq", function() h.notify.error "use <C-y> instead!" end)
 
 h.keys.map("n", "<leader>gh", h.keys.vim_cmd_cb "DiffviewFileHistory %", { desc = "", })
