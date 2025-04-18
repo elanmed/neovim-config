6f2ff7941f5b29e936e3b17e14648169286bfb7a
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-04-18 11:56:16 -0400

    cleanup custom highlights

--- lua/feature_complete/plugins/colorscheme.lua
+++ lua/feature_complete/plugins/colorscheme.lua
@@ -37,22 +37,14 @@ local M = {
 }
 
 vim.api.nvim_set_hl(h.curr.namespace, "MatchParen", { fg = nil, bg = M.grey, })
-vim.api.nvim_set_hl(h.curr.namespace, "LspReferenceRead", { bg = "#2e3136", })  -- between base01 and base 02
-vim.api.nvim_set_hl(h.curr.namespace, "LspReferenceText", { bg = "#2e3136", })  -- between base01 and base 02
-vim.api.nvim_set_hl(h.curr.namespace, "LspReferenceWrite", { bg = "#2e3136", }) -- between base01 and base 02
+vim.api.nvim_set_hl(h.curr.namespace, "LspReferenceRead", { bg = "#2e3136", }) -- between base01 and base 02
+vim.api.nvim_set_hl(h.curr.namespace, "LspReferenceText", { bg = "#2e3136", })
+vim.api.nvim_set_hl(h.curr.namespace, "LspReferenceWrite", { bg = "#2e3136", })
 
-vim.api.nvim_set_hl(h.curr.namespace, "PmenuBorder", { fg = M.light_grey, })
-vim.api.nvim_set_hl(h.curr.namespace, "TelescopeBorder", { link = "PmenuBorder", })
-vim.api.nvim_set_hl(h.curr.namespace, "FloatBorder", { link = "PmenuBorder", }) -- harpoon border
-vim.api.nvim_set_hl(h.curr.namespace, "CursorLine", { link = "Visual", })
-vim.api.nvim_set_hl(h.curr.namespace, "AerialLine", { link = "Visual", })
 vim.api.nvim_set_hl(h.curr.namespace, "WilderAccent", { fg = M.orange, })
 vim.api.nvim_set_hl(h.curr.namespace, "WildMenu", { fg = M.yellow, underline = true, bold = true, })
 
-vim.api.nvim_set_hl(h.curr.namespace, "FlashMatch", { link = "Search", })
 vim.api.nvim_set_hl(h.curr.namespace, "FlashLabel", { link = "IncSearch", })
-vim.api.nvim_set_hl(h.curr.namespace, "FlashCurrent", { bg = M.red, fg = colors.base01, })
-vim.api.nvim_set_hl(h.curr.namespace, "FlashCurrent", { bg = M.red, fg = colors.base01, })
 
 vim.api.nvim_set_hl(h.curr.namespace, "SnacksIndent", { fg = M.grey, })
 vim.api.nvim_set_hl(h.curr.namespace, "SnacksIndentScope", { fg = M.light_grey, })
