b5ea616d422aa74427df54bfb7742c5206208490
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-04-18 11:43:33 -0400

    enable snacks scroll

--- lua/feature_complete/plugins/misc.lua
+++ lua/feature_complete/plugins/misc.lua
@@ -1,23 +1,25 @@
 local h = require "shared.helpers"
+local snacks = require "snacks"
 
 --- @type snacks.Config
-require "snacks".setup {
+snacks.setup {
   indent = { enabled = true, animate = { enabled = false, }, },
   explorer = { enabled = true, replace_netrw = false, },
+  scroll = { enabled = true, },
+  picker = {
+    win = {
+      input = {
+        keys = {
+          ["<Esc>"] = { "close", mode = "i", },
+          ["<C-c>"] = { "cancel", mode = "i", },
+        },
+      },
+      list = {
+        keys = {
+          ["<C-u>"] = { "preview_scroll_up", mode = { "i", "n", }, },
+          ["<C-d>"] = { "preview_scroll_down", mode = { "i", "n", }, },
+        },
+      },
+    },
+  },
 }
-
-h.keys.map("n", "<leader>tm", function()
-  vim.cmd "UndotreeToggle"
-  vim.cmd "wincmd h"
-end, { desc = "Toggle undotree", })
-
-vim.api.nvim_create_autocmd({ "BufEnter", }, {
-  pattern = "*",
-  callback = function()
-    if h.tbl.contains_value({ "aerial", "undotree", }, vim.bo.filetype) then
-      h.set.cursorline = true
-    else
-      h.set.cursorline = false
-    end
-  end,
-})
