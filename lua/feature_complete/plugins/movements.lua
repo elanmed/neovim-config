19722438dba8d0fbed991dfc7c05c085b59dabf5
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-04-26 15:59:04 -0400

    update <c-d>, <c-u> remaps to use M when at the top, bottom of the buffer

--- lua/feature_complete/plugins/movements.lua
+++ lua/feature_complete/plugins/movements.lua
@@ -15,8 +15,20 @@ local function cinnamon_scroll_cb(movement)
   return function() cinnamon.scroll(movement) end
 end
 
-vim.keymap.set({ "n", "v", "i", }, "<C-u>", cinnamon_scroll_cb "<C-u>")
-vim.keymap.set({ "n", "v", "i", }, "<C-d>", cinnamon_scroll_cb "<C-d>")
+vim.keymap.set({ "n", "v", "i", }, "<C-u>", function()
+  if vim.fn.line "." == vim.fn.line "$" then
+    cinnamon.scroll "M"
+  else
+    cinnamon.scroll "<C-u>"
+  end
+end)
+vim.keymap.set({ "n", "v", "i", }, "<C-d>", function()
+  if vim.fn.line "." == 1 then
+    cinnamon.scroll "M"
+  else
+    cinnamon.scroll "<C-d>"
+  end
+end)
 vim.keymap.set("n", "n", cinnamon_scroll_cb "n")
 vim.keymap.set("n", "N", cinnamon_scroll_cb "N")
 vim.keymap.set("n", "}", cinnamon_scroll_cb "}")
@@ -25,7 +37,6 @@ vim.keymap.set("n", "{", cinnamon_scroll_cb "{")
 require "multicursors".setup {
   hint_config = false,
 }
--- TODO: j, k
 vim.keymap.set("n", "<leader>tm", function()
   vim.cmd "MCstart"
   h.notify.info "Starting multicursor"
@@ -53,7 +64,7 @@ flash.setup {
 vim.keymap.set("n", "s", function() flash.jump { forward = true, } end)
 vim.keymap.set("n", "S", function() flash.jump { forward = false, } end)
 vim.keymap.set("n", "<leader>v", function() flash.treesitter() end)
-vim.keymap.set("n", "<leader>sa", function()
+vim.keymap.set("n", "<leader>s", function()
   -- https://github.com/folke/flash.nvim#-examples
   flash.jump {
     forward = true,
