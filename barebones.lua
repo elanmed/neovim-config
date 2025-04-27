51356b3be076d06f1de4d11d269f4e5472d91892
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-04-27 15:06:27 -0400

    use default nvim theme in barebones

--- barebones.lua
+++ barebones.lua
@@ -1,7 +1,6 @@
 -- remap leader before importing remaps that use it
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.cmd "colorscheme slate"
 
 require "shared.options"
 require "shared.remaps"
