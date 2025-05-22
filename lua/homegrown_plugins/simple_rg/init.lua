4b35b934ba6aa23c585e398e61aad045c87843f8
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-05-21 23:15:56 -0400

    refactor simple rg

--- lua/homegrown_plugins/simple_rg/init.lua
+++ lua/homegrown_plugins/simple_rg/init.lua
@@ -30,31 +30,29 @@ end
 
 --- @param opts { dir_tbl: table, file_tbl: table, ext_tbl: table, negate: boolean }
 local function construct_rg_flag(opts)
-  local dir_tbl, file_tbl, ext_tbl, negate = opts.dir_tbl, opts.file_tbl, opts.ext_tbl, opts.negate
-
   local ext_tbl_prefixed = vim.tbl_map(function(ext)
     return "*." .. ext
-  end, ext_tbl)
+  end, opts.ext_tbl)
 
-  local file_and_ext_tbl = vim.list_extend(vim.deepcopy(file_tbl), ext_tbl_prefixed)
+  local file_and_ext_tbl = vim.list_extend(vim.deepcopy(opts.file_tbl), ext_tbl_prefixed)
 
   local flag = ""
-  if #dir_tbl > 0 then
-    flag = flag .. "'**/{" .. table.concat(dir_tbl, ",") .. "}/**"
+  if #opts.dir_tbl > 0 then
+    flag = flag .. "'**/{" .. table.concat(opts.dir_tbl, ",") .. "}/**"
     if #file_and_ext_tbl == 0 then
       flag = flag .. "'"
     end
   end
 
   if #file_and_ext_tbl > 0 then
-    if #dir_tbl == 0 then
+    if #opts.dir_tbl == 0 then
       flag = flag .. "'**"
     end
     flag = flag .. "/{" .. table.concat(file_and_ext_tbl, ",") .. "}'"
   end
 
   if #flag > 0 then
-    if negate then
+    if opts.negate then
       flag = "!" .. flag
     end
 
@@ -69,7 +67,54 @@ local function parse_search(prompt)
   local end_tilde_index = prompt:find("~", 2)
   local end_index = end_tilde_index or (#prompt + 1)
   local search = prompt:sub(2, end_index - 1)
-  return { search = ("'%s'"):format(search), search_index = end_index, }
+  return { search = ("'%s'"):format(search), search_end_index = end_index, }
+end
+
+--- @param tokens table
+local function parse_flags(tokens)
+  local state = nil
+  local parsed = {
+    include_file = {},
+    negate_file = {},
+    include_dir = {},
+    negate_dir = {},
+    include_ext = {},
+    negate_ext = {},
+    case_flag = { "--ignore-case", },
+    word_flag = { nil, },
+  }
+
+  for _, token in ipairs(tokens) do
+    if token == "-c" then
+      parsed.case_flag = { "--case-sensitive", }
+      state = nil
+    elseif token == "-nc" then
+      parsed.case_flag = { "--ignore-case", }
+      state = nil
+    elseif token == "-w" then
+      parsed.word_flag = { "--word-regexp", }
+      state = nil
+    elseif token == "-nw" then
+      parsed.word_flag = { nil, }
+      state = nil
+    elseif token == "-f" then
+      state = "file"
+    elseif token == "-d" then
+      state = "dir"
+    elseif token == "-e" then
+      state = "ext"
+    elseif state then
+      local include_key = "include_" .. state
+      local negate_key = "negate_" .. state
+      record_custom_flag {
+        str = token,
+        include_tbl = parsed[include_key],
+        negate_tbl = parsed[negate_key],
+      }
+    end
+  end
+
+  return parsed
 end
 
 M.construct_simple_rg = function(prompt)
@@ -77,106 +122,38 @@ M.construct_simple_rg = function(prompt)
     return nil
   end
 
-  local parsing_file_flags = false
-  local parsing_dir_flags = false
-  local parsing_ext_flags = false
-
-  local include_file_flags = {}
-  local negate_file_flags = {}
-  local include_dir_flags = {}
-  local negate_dir_flags = {}
-  local include_ext_flags = {}
-  local negate_ext_flags = {}
-  local case_sensitive_flag = { "--ignore-case", }
-  local whole_word_flag = { nil, }
-
   local parsed_search = parse_search(prompt)
-  local search, search_index = parsed_search.search, parsed_search.search_index
 
-  local flags_prompt = prompt:sub(search_index + 1)
-  local split_flags_prompt = split(flags_prompt)
-
-  local flags_index = 1
-  while flags_index < (#split_flags_prompt + 1) do
-    local flag_token = split_flags_prompt[flags_index]
-
-    local is_last_char_space = flags_prompt:sub(#flags_prompt, #flags_prompt) == " "
-    if flags_index == #split_flags_prompt and not is_last_char_space then
-      -- avoid updating the rg command
-      return nil
-    end
-
-    if flag_token == "-c" then
-      case_sensitive_flag = { "--case-sensitive", }
-      goto continue
-    end
-
-    if flag_token == "-nc" then
-      case_sensitive_flag = { "--ignore-case", }
-      goto continue
-    end
-
-    if flag_token == "-w" then
-      whole_word_flag = { "--word-regexp", }
-      goto continue
-    end
-
-    if flag_token == "-nw" then
-      whole_word_flag = { nil, }
-      goto continue
-    end
-
-    if flag_token == "-f" then
-      parsing_file_flags = true
-      parsing_dir_flags = false
-      parsing_ext_flags = false
-      goto continue
-    end
-
-    if flag_token == "-d" then
-      parsing_dir_flags = true
-      parsing_file_flags = false
-      parsing_ext_flags = false
-      goto continue
-    end
-
-    if flag_token == "-e" then
-      parsing_ext_flags = true
-      parsing_dir_flags = false
-      parsing_file_flags = false
-      goto continue
-    end
-
-    if parsing_dir_flags == true then
-      record_custom_flag { str = flag_token, include_tbl = include_dir_flags, negate_tbl = negate_dir_flags, }
-      goto continue
-    end
-
-    if parsing_file_flags == true then
-      record_custom_flag { str = flag_token, include_tbl = include_file_flags, negate_tbl = negate_file_flags, }
-      goto continue
-    end
-
-    if parsing_ext_flags == true then
-      record_custom_flag { str = flag_token, include_tbl = include_ext_flags, negate_tbl = negate_ext_flags, }
-      goto continue
-    end
-
-
-    ::continue::
-    flags_index = flags_index + 1
+  local flags_prompt = prompt:sub(parsed_search.search_end_index + 1)
+  if flags_prompt:sub(-1) ~= " " then
+    return nil
   end
 
+  local tokens = split(flags_prompt)
+  local flags = parse_flags(tokens)
 
-  local include_flag = construct_rg_flag { negate = false, dir_tbl = include_dir_flags, file_tbl = include_file_flags, ext_tbl = include_ext_flags, }
-  local negate_flag = construct_rg_flag { negate = true, dir_tbl = negate_dir_flags, file_tbl = negate_file_flags, ext_tbl = negate_ext_flags, }
+  local include_flag = construct_rg_flag {
+    negate = false,
+    dir_tbl = flags.include_dir,
+    file_tbl = flags.include_file,
+    ext_tbl = flags.include_ext,
+  }
+
+  local negate_flag = construct_rg_flag {
+    negate = true,
+    dir_tbl = flags.negate_dir,
+    file_tbl = flags.negate_file,
+    ext_tbl = flags.negate_ext,
+  }
 
   local cmd = flatten {
     "rg",
     "--line-number", "--column", "--no-heading", -- formatting for fzf-lua
     "--hidden",
     "--color=always",
-    case_sensitive_flag, whole_word_flag, search, include_flag, negate_flag,
+    flags.case_flag, flags.word_flag,
+    parsed_search.search,
+    include_flag, negate_flag,
   }
 
   return table.concat(cmd, " ")
