af8fd47882995e42ddc25cb8fbff563c6c2fc809
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-08 23:02:22 -0400

    move qf list remaps to the plugin dir

--- lua/feature_complete/plugins/quickfix_list.lua
+++ lua/feature_complete/plugins/quickfix_list.lua
@@ -1,3 +1,5 @@
+local h = require "shared.helpers"
+
 -- require "quickfix-preview".setup {
 --   pedit_prefix = "vertical rightbelow",
 --   pedit_postfix = "| wincmd =",
@@ -18,7 +20,8 @@
 --   },
 -- }
 
-require "homegrown_plugins.quickfix_preview.init".setup {
+local qf_preview = require "homegrown_plugins.quickfix_preview.init"
+qf_preview.setup {
   keymaps = {
     select_close_preview = "o",
     select_close_quickfix = "<cr>",
@@ -34,5 +37,59 @@ require "homegrown_plugins.quickfix_preview.init".setup {
   get_open_win_opts = function()
     return { border = "rounded", }
   end,
-
 }
+
+--- @param predicate fun(curr_item, idx: number): boolean
+--- @param list table
+local function filter(predicate, list)
+  local filtered_list = {}
+  for index, val in pairs(list) do
+    if (predicate(val, index)) then -- vim.tbl_filter doesn't pass the index
+      table.insert(filtered_list, val)
+    end
+  end
+  return filtered_list
+end
+
+vim.api.nvim_create_autocmd({ "FileType", }, {
+  callback = function()
+    if vim.bo.buftype ~= "quickfix" then return end
+
+    vim.keymap.set("n", "<leader>d", function()
+      qf_preview.close()
+      vim.fn.setqflist({}, "f") -- clear all
+      -- vim.fn.setqflist({}, "r") -- clear current
+    end, { buffer = true, desc = "Clear all quickfix lists", })
+
+    vim.keymap.set("n", "dd", function()
+      local curr_line = vim.fn.line "."
+      local qf_list = vim.fn.getqflist()
+      local is_last_line = curr_line == #qf_list
+
+      local filtered_qf_list = filter(function(_, idx)
+        return idx ~= curr_line
+      end, qf_list)
+
+      vim.fn.setqflist(filtered_qf_list, "r")
+      if #qf_list == 1 then
+        qf_preview.close()
+        return
+      end
+      vim.api.nvim_win_set_cursor(h.curr.window, { is_last_line and curr_line - 1 or curr_line, 0, })
+    end, { buffer = true, desc = "Delete the current qf item", })
+
+    vim.keymap.set("n", ">", function()
+      local success = pcall(vim.cmd, "cnewer")
+      if not success then
+        h.notify.warn "No newer list!"
+      end
+    end, { buffer = true, desc = "Go to the next quickfix list", })
+
+    vim.keymap.set("n", "<", function()
+      local success = pcall(vim.cmd, "colder")
+      if not success then
+        h.notify.warn "No older list!"
+      end
+    end, { buffer = true, desc = "Go to the pre quickfix list", })
+  end,
+})
