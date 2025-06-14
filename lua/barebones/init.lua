7f4a43d3ee5e8ffd810f512146e2d29d3659acda
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-14 18:34:12 -0400

    basic buffer selection for barebones

--- lua/barebones/init.lua
+++ lua/barebones/init.lua
@@ -24,8 +24,26 @@ vim.keymap.set("n", "<leader>f", ":grep<space>")
 vim.keymap.set("n", "<leader>a", ":grep<space>")
 vim.keymap.set("n", "<leader>h", ":help<space>")
 vim.keymap.set("n", "<leader>m", h.keys.vim_cmd_cb "marks")
-vim.keymap.set("n", "<leader>b", h.keys.vim_cmd_cb "buffers")
 vim.keymap.set("n", "<leader>l;", h.keys.vim_cmd_cb "history")
+vim.keymap.set("n", "<leader>b", function()
+  local buffers = vim.api.nvim_list_bufs()
+
+  local items = {}
+  for _, buf in ipairs(buffers) do
+    if vim.api.nvim_buf_is_loaded(buf) then
+      local name = vim.api.nvim_buf_get_name(buf)
+      if name ~= "" and name ~= "[No Name]" then
+        table.insert(items, { buf = buf, name = name, })
+      end
+    end
+  end
+
+  vim.ui.select(
+    items,
+    { prompt = "Select buffer:", format_item = function(item) return item.name end, },
+    function(choice) if choice then vim.api.nvim_set_current_buf(choice.buf) end end
+  )
+end)
 
 vim.keymap.set("c", "/", function()
   if vim.fn.wildmenumode() == 1 then
@@ -34,3 +52,22 @@ vim.keymap.set("c", "/", function()
     return "/"
   end
 end, { expr = true, })
+
+local qf_preview = require "homegrown_plugins.quickfix_preview.init"
+qf_preview.setup {
+  keymaps = {
+    select_close_preview = "o",
+    select_close_quickfix = "<cr>",
+    toggle = "t",
+    next = { key = "<C-n>", },
+    prev = { key = "<C-p>", },
+    cnext = { key = "]q", },
+    cprev = { key = "[q", },
+  },
+  get_preview_win_opts = function()
+    return { relativenumber = false, number = true, signcolumn = "no", cursorline = true, winblend = 5, }
+  end,
+  get_open_win_opts = function()
+    return { border = "rounded", }
+  end,
+}
