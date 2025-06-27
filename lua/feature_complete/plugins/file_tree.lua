cf5603d13b4b189fff46756c4d38282b8ac0c0ee
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-27 09:02:35 -0400

    switch <leader>i to fzf git hunks

--- lua/feature_complete/plugins/file_tree.lua
+++ lua/feature_complete/plugins/file_tree.lua
@@ -3,7 +3,7 @@ local snacks = require "snacks"
 
 vim.keymap.set("n", "<C-f>", h.keys.vim_cmd_cb "Oil", { desc = "Toggle oil", })
 vim.keymap.set("n", "-", h.keys.vim_cmd_cb "Oil", { desc = "Toggle oil", })
-vim.keymap.set("n", "<leader>i", snacks.explorer.reveal, { desc = "Open snacks explorer", })
+vim.keymap.set("n", "<leader>ti", snacks.explorer.reveal, { desc = "Open snacks explorer", })
 
 require "oil".setup {
   default_file_explorer = true,
