f8656c76cdd7b03dbb91a1e1282842c0f2a3e537
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-26 22:13:04 -0400

    replace fzf git branches with fzf git hunks

--- lua/feature_complete/plugins/fzf.lua
+++ lua/feature_complete/plugins/fzf.lua
@@ -70,8 +70,8 @@ vim.keymap.set("n", "<leader>h", with_preview_cb(fzf_lua.helptags), { desc = "Se
 vim.keymap.set("n", "<leader>m", with_preview_cb(fzf_lua.marks), { desc = "Search help tags with fzf", })
 vim.keymap.set("n", "<leader>l;", without_preview_cb(fzf_lua.command_history),
   { desc = "Search search history with fzf", })
-vim.keymap.set("n", "<leader>lg", without_preview_cb(fzf_lua.git_branches),
-  { desc = "Search git branches with fzf", })
+vim.keymap.set("n", "<leader>lg", without_preview_cb(fzf_lua.git_hunks),
+  { desc = "Search git hunks with fzf", })
 vim.keymap.set("n", "<leader>b", without_preview_cb(fzf_lua.buffers),
   { desc = "Search currently open buffers with fzf", })
 vim.keymap.set("n", "<leader>f",
