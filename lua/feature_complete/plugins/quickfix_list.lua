88ab0887decf276936316f82ee829af4a4b9acde
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-26 21:19:34 -0400

    remove local quickfix preview plugin

--- lua/feature_complete/plugins/quickfix_list.lua
+++ lua/feature_complete/plugins/quickfix_list.lua
@@ -20,25 +20,6 @@ require "quickfix-preview".setup {
   },
 }
 
--- local qf_preview = require "homegrown_plugins.quickfix_preview.init"
--- qf_preview.setup {
---   keymaps = {
---     select_close_preview = "o",
---     select_close_quickfix = "<cr>",
---     toggle = "t",
---     next = { key = "<C-n>", },
---     prev = { key = "<C-p>", },
---     cnext = { key = "]q", },
---     cprev = { key = "[q", },
---   },
---   get_preview_win_opts = function()
---     return { relativenumber = false, number = true, signcolumn = "no", cursorline = true, winblend = 5, }
---   end,
---   get_open_win_opts = function()
---     return { border = "rounded", }
---   end,
--- }
-
 --- @param predicate fun(curr_item, idx: number): boolean
 --- @param list table
 local function filter(predicate, list)
