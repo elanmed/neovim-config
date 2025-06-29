3224a51950a2798e9b8fdc23252ec4cab5db892f
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-29 18:09:47 -0400

    use default grug far remaps

--- lua/feature_complete/plugins/far.lua
+++ lua/feature_complete/plugins/far.lua
@@ -3,7 +3,7 @@ local grug = require "grug-far"
 
 local default_instance = "default_instance"
 
-vim.keymap.set("n", "<leader>re", function()
+vim.keymap.set("n", "<leader>r", function()
   if grug.has_instance(default_instance) then
     grug.get_instance(default_instance):open { startInInsertMode = false, }
   else
@@ -15,24 +15,9 @@ end, { desc = "Open the grug-far ui", })
 
 grug.setup {
   keymaps = {
-    replace = false,
-    qflist = { n = "<leader>rq", },
-    syncLocations = { n = "<leader>rs", },
-    syncLine = { n = "<leader>rl", },
-    close = { n = "<leader>re", },
-    historyOpen = { n = "<leader>rh", },
-    historyAdd = false,
+    qflist = { n = "<leader>f", },
+    close = { n = "<leader>q", },
     refresh = { n = "<leader>e", },
-    openLocation = { n = "<leader>ro", },
-    openNextLocation = { n = "<C-j>", },
-    openPrevLocation = { n = "<C-k>", },
-    gotoLocation = { n = "<enter>", },
-    pickHistoryEntry = { n = "<enter>", },
-    abort = false,
-    help = { n = "g?", },
-    toggleShowCommand = false,
-    swapEngine = false,
-    previewLocation = { n = "<leader>rp", },
-    swapReplacementInterpreter = false,
+    engine = { n = "<leader>g", },
   },
 }
