aa31d908cc007792ef5731878ddc75d036761fb8
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-06-29 18:10:20 -0400

    add <leader>x to clear all qf lists, <leader>d to clear the current, remove dd

--- lua/feature_complete/plugins/quickfix_list.lua
+++ lua/feature_complete/plugins/quickfix_list.lua
@@ -24,27 +24,14 @@ vim.api.nvim_create_autocmd({ "FileType", }, {
   callback = function()
     if vim.bo.buftype ~= "quickfix" then return end
 
-    vim.keymap.set("n", "<leader>d", function()
+    vim.keymap.set("n", "<leader>x", function()
       vim.cmd "pclose"
-      vim.fn.setqflist({}, "f") -- clear all
-      -- vim.fn.setqflist({}, "r") -- clear current
+      vim.fn.setqflist({}, "f")
     end, { buffer = true, desc = "Clear all quickfix lists", })
 
-    vim.keymap.set("n", "dd", function()
-      local curr_line = vim.fn.line "."
-      local qf_list = vim.fn.getqflist()
-      local is_last_line = curr_line == vim.tbl_count(qf_list)
-
-      local filtered_qf_list = h.tbl.filter(function(_, idx)
-        return idx ~= curr_line
-      end, qf_list)
-
-      vim.fn.setqflist(filtered_qf_list, "r")
-      if vim.tbl_count(qf_list) == 1 then
-        vim.cmd "pclose"
-        return
-      end
-      vim.api.nvim_win_set_cursor(h.curr.window, { is_last_line and curr_line - 1 or curr_line, 0, })
-    end, { buffer = true, desc = "Delete the current qf item", })
+    vim.keymap.set("n", "<leader>d", function()
+      vim.cmd "pclose"
+      vim.fn.setqflist({}, "r")
+    end, { buffer = true, desc = "Clear the current quickfix list", })
   end,
 })
