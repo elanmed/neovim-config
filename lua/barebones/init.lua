a210a4fed2b34086c97e55ba02e1ebe33524026d
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-05 21:57:31 -0400

    update barebones remaps

--- lua/barebones/init.lua
+++ lua/barebones/init.lua
@@ -18,13 +18,13 @@ vim.keymap.set("n", "<C-f>", function()
   end
 end, { desc = "Toggle netrw, focusing the current buffer", })
 
-vim.keymap.set("n", "<C-p>", ":find<space>")
+vim.keymap.set("n", "<leader>f", ":find<space>")
 
 vim.opt.path:append "**" -- search in subdirectories
 vim.keymap.set("n", "<leader>a", ":grep<space>")
 vim.keymap.set("n", "<leader>h", ":help<space>")
-vim.keymap.set("n", "<leader>lm", h.keys.vim_cmd_cb "marks")
-vim.keymap.set("n", "<leader>l;", h.keys.vim_cmd_cb "history")
+vim.keymap.set("n", "<leader>zm", h.keys.vim_cmd_cb "marks")
+vim.keymap.set("n", "<leader>z;", h.keys.vim_cmd_cb "history")
 vim.keymap.set("n", "<leader>b", ":b<space>")
 
 vim.keymap.set("c", "/", function()
