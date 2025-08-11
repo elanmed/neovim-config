2aad16afd7a81a942bc69d5f87ed4ab28553222d
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-10 22:37:46 -0400

    add yobi vim remap

--- lua/barebones/init.lua
+++ lua/barebones/init.lua
@@ -8,7 +8,7 @@ vim.opt.scrolloff = 999
 
 -- removing banner causes a bug where the terminal flickers
 -- vim.g.netrw_banner = 0 -- removes banner at the top
-vim.g.netrw_liststyle = 0
+vim.g.netrw_liststyle = 3
 
 vim.keymap.set("n", "<C-f>", function()
   if vim.bo.filetype == "netrw" then
@@ -25,7 +25,9 @@ vim.keymap.set("n", "<leader>a", ":grep<space>")
 vim.keymap.set("n", "<leader>h", ":help<space>")
 vim.keymap.set("n", "<leader>zm", h.keys.vim_cmd_cb "marks")
 vim.keymap.set("n", "<leader>z;", h.keys.vim_cmd_cb "history")
-vim.keymap.set("n", "<leader>b", ":b<space>")
+vim.keymap.set("n", "<leader>b", ":buffer<space>")
+vim.keymap.set("n", "<C-n>", h.keys.vim_cmd_cb "cnext")
+vim.keymap.set("n", "<C-p>", h.keys.vim_cmd_cb "cprev")
 
 vim.keymap.set("c", "/", function()
   if vim.fn.wildmenumode() == 1 then
@@ -34,3 +36,15 @@ vim.keymap.set("c", "/", function()
     return "/"
   end
 end, { expr = true, })
+
+-- https://yobibyte.github.io/vim.html
+vim.keymap.set("n", "<space>ze", function()
+  vim.ui.input({ prompt = "$ "}, function(cmd)
+    if cmd and cmd ~= "" then
+      vim.cmd "vnew"
+      vim.bo.buftype = "nofile"
+      vim.bo.bufhidden = "wipe"
+      vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(cmd))
+    end
+  end)
+end)
