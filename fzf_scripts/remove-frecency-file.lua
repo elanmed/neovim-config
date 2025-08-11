e316523a16a00057a756cbd2e87769f8b5f45677
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-10 21:31:35 -0400

    misc polish

--- fzf_scripts/remove-frecency-file.lua
+++ fzf_scripts/remove-frecency-file.lua
@@ -4,7 +4,5 @@ assert(cwd)
 local rel_path = arg[2]
 assert(rel_path)
 
-
-local frecency_algo = require "fzf-lua-frecency.algo"
 local abs_path = vim.fs.joinpath(cwd, rel_path)
-frecency_algo.update_file_score(abs_path, { update_type = "remove", })
+require "fzf-lua-frecency.algo".update_file_score(abs_path, { update_type = "remove", })
