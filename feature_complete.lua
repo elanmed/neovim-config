6815d05fab3a05c9b945caf00f6b4ef77458e672
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-12 00:17:30 -0400

    require cleanup

--- feature_complete.lua
+++ feature_complete.lua
@@ -1,8 +1,6 @@
-local h = require "helpers"
-
 -- remap leader before importing remaps that use it
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-h.require_dir "shared"
+require "helpers".require_dir "shared"
 require "feature_complete"
