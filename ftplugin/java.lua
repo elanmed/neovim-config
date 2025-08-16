632e3d318ac2758991e77cede1baf091281c972e
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-16 17:30:41 -0400

    update jdtls path

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,6 @@
 -- https://github.com/mfussenegger/nvim-jdtls#configuration-quickstart
 local config = {
-  cmd = { "/path/to/jdt-language-server/bin/jdtls", },
+  cmd = { "jdtls", },
   root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw", }, { upward = true, })[1]),
 }
 require "jdtls".start_or_attach(config)
