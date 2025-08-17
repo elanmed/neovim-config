5c6e47c0b871dfc50bb2f23accb405750decd5fc
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-16 22:31:27 -0400

    add message when no marks

--- fzf_scripts/get_marks.lua
+++ fzf_scripts/get_marks.lua
@@ -14,9 +14,15 @@ vim.fn.chanclose(chan)
 
 local global_marks = ("abcdefghijklmnopqrstuvwxyz"):upper()
 
+local num_marks = 0
 for _, mark in pairs(mark_list) do
   local name = mark.mark:sub(2, 2)
   if not global_marks:find(name) then goto continue end
   h.print_with_flush(("%s|%s"):format(name, mark.file))
+  num_marks = num_marks + 1
   ::continue::
+end
+
+if num_marks == 0 then
+  h.print_with_flush "No marks!"
 end
