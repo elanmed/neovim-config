3662e394d18660cab915b657759f2c4ad28f1bf3
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-17 23:29:09 -0400

    add boost for open bufs, changed bufs, alt bufs

--- fzf_scripts/get_smart_fzy_files.lua
+++ fzf_scripts/get_smart_fzy_files.lua
@@ -8,34 +8,55 @@ query = query:gsub("%s+", "") -- fzy doesn't ignore spaces
 local CYAN = "\27[36m"
 local RESET = "\27[0m"
 
+local OPEN_BUF_BOOST = 10
+local CHANGED_BUF_BOOST = 20
+local ALT_BUF_BOOST = 25
+
+local h = require "helpers"
 local chan = vim.fn.sockconnect("pipe", servername, { rpc = true, })
+
 --- @type string
 local cwd = vim.rpcrequest(chan, "nvim_call_function", "getcwd", {})
+
+--- @class BufInfo
+--- @field name string
+--- @field loaded number
+--- @field changed boolean
+
+--- @type BufInfo[]
+local open_buffers = vim.rpcrequest(chan, "nvim_call_function", "getbufinfo", { { buflisted = h.vimscript_true, }, })
+
+--- @type string
+local alternate_file = vim.rpcrequest(chan, "nvim_call_function", "expand", { "#:p", })
+h.dev.log { alternate_file = alternate_file, }
 vim.fn.chanclose(chan)
 
 local mini_icons = require "mini.icons"
 local fzy = require "fzy-lua-native"
-local h = require "helpers"
 local frecency_helpers = require "fzf-lua-frecency.helpers"
 local sorted_files_path = frecency_helpers.get_sorted_files_path()
 local frecency_fs = require "fzf-lua-frecency.fs"
 local frecency_algo = require "fzf-lua-frecency.algo"
 
 local now = os.time()
---- @param abs_file string
---- @param score number
---- @param pos table
-local function format_filename(abs_file, score, pos)
-  local icon_ok, icon_res = pcall(mini_icons.get, "file", abs_file)
+
+--- @class FormatFilenameOpts
+--- @field abs_file string
+--- @field score number
+--- @field pos table
+
+--- @param opts FormatFilenameOpts
+local function format_filename(opts)
+  local icon_ok, icon_res = pcall(mini_icons.get, "file", opts.abs_file)
   local icon = icon_ok and icon_res or "?"
   --- @type string
-  local rel_file = vim.fs.relpath(cwd, abs_file)
+  local rel_file = vim.fs.relpath(cwd, opts.abs_file)
 
   local max_score = 99
   local max_score_len = #frecency_helpers.exact_decimals(max_score, 2)
 
   local formatted_score = frecency_helpers.pad_str(
-    frecency_helpers.fit_decimals(score or 0, max_score_len),
+    frecency_helpers.fit_decimals(opts.score or 0, max_score_len),
     max_score_len
   )
 
@@ -43,7 +64,7 @@ local function format_filename(abs_file, score, pos)
   local str_ptr = 1
   local pos_ptr = 1
   while str_ptr < #rel_file + 1 do
-    if str_ptr == pos[pos_ptr] then
+    if str_ptr == opts.pos[pos_ptr] then
       with_ansi = with_ansi .. CYAN .. rel_file:sub(str_ptr, str_ptr) .. RESET
       pos_ptr = pos_ptr + 1
     else
@@ -55,44 +76,57 @@ local function format_filename(abs_file, score, pos)
   return ("%s %s |%s"):format(formatted_score, icon, with_ansi)
 end
 
-local frecency_scored_files = {}
+local scored_files = {}
 
-local seen = {}
 local dated_files_path = frecency_helpers.get_dated_files_path()
 local dated_files = frecency_fs.read(dated_files_path)
-
-for frecency_file in io.lines(sorted_files_path) do
-  if vim.fn.filereadable(frecency_file) == 0 then goto continue end
-  if not vim.startswith(frecency_file, cwd) then goto continue end
-  seen[frecency_file] = true
-
-  local db_index = 1 -- TODO: remove
-  if not dated_files[db_index] then
-    dated_files[db_index] = {}
-  end
-  local date_at_score_one = dated_files[db_index][frecency_file]
-  local score = frecency_algo.compute_score { now = now, date_at_score_one = date_at_score_one, }
-  frecency_scored_files[frecency_file] = score
-
-  ::continue::
-end
 
 local fd_cmd = "fd --absolute-path --hidden --type f --exclude node_modules --exclude .git --exclude dist"
 local handle = io.popen(fd_cmd)
 if handle then
   for fd_file in handle:lines() do
-    if seen[fd_file] then goto continue end
-    seen[fd_file] = true
-
-    frecency_scored_files[fd_file] = 0
-
-    ::continue::
+    scored_files[fd_file] = 0
   end
   handle:close()
 end
 
+if alternate_file and alternate_file ~= "" and vim.fn.filereadable(alternate_file) == h.vimscript_true then
+  scored_files[alternate_file] = (scored_files[alternate_file] or 0) + ALT_BUF_BOOST
+end
+
+for _, buf in ipairs(open_buffers) do
+  if buf.loaded == h.vimscript_false then goto continue end
+  if not vim.startswith(buf.name, cwd) then goto continue end
+  if not buf.name then goto continue end
+  if buf.name == "" then goto continue end
+
+  if buf.changed == h.vimscript_true then
+    scored_files[buf.name] = scored_files[buf.name] + CHANGED_BUF_BOOST
+  else
+    scored_files[buf.name] = (scored_files[buf.name] or 0) + OPEN_BUF_BOOST
+  end
+
+  ::continue::
+end
+
+for frecency_file in io.lines(sorted_files_path) do
+  if vim.fn.filereadable(frecency_file) == 0 then goto continue end
+  if not vim.startswith(frecency_file, cwd) then goto continue end
+
+  local db_index = 1
+  if not dated_files[db_index] then
+    dated_files[db_index] = {}
+  end
+  local date_at_score_one = dated_files[db_index][frecency_file]
+  local score = frecency_algo.compute_score { now = now, date_at_score_one = date_at_score_one, }
+
+  scored_files[frecency_file] = (scored_files[frecency_file] or 0) + score
+
+  ::continue::
+end
+
 local weighted_files = {}
-for abs_file, frecency_score in pairs(frecency_scored_files) do
+for abs_file, frecency_score in pairs(scored_files) do
   local rel_file = vim.fs.relpath(cwd, abs_file)
   local fzy_score = 0
   local fzy_pos = {}
@@ -100,10 +134,9 @@ for abs_file, frecency_score in pairs(frecency_scored_files) do
   if query ~= "" and fzy.has_match(query, rel_file) then
     fzy_score = fzy.score(query, rel_file)
     fzy_pos = fzy.positions(query, rel_file)
-    h.dev.log { query = query, fzy = fzy.score(query, rel_file), file = rel_file, pos = fzy_pos, }
   end
 
-  local score = fzy_score * 1.5 + frecency_score
+  local score = fzy_score * 3 + frecency_score
   table.insert(weighted_files, { file = abs_file, score = score, pos = fzy_pos, })
 end
 
@@ -112,6 +145,10 @@ table.sort(weighted_files, function(a, b)
 end)
 
 for _, weighted_entry in pairs(weighted_files) do
-  local formatted = format_filename(weighted_entry.file, weighted_entry.score, weighted_entry.pos)
+  local formatted = format_filename {
+    abs_file = weighted_entry.file,
+    pos = weighted_entry.pos,
+    score = weighted_entry.score,
+  }
   h.print_with_flush(formatted)
 end
