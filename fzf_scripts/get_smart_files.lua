36fabc5d568d748f86add0ae2fca022a3a6d135f
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-08-20 00:27:33 -0400

    remove benchmarking

--- fzf_scripts/get_smart_files.lua
+++ fzf_scripts/get_smart_files.lua
@@ -1,14 +1,6 @@
 assert(arg[1], "Missing arg1: `servername`")
 
-local entire_file_time = os.clock()
-
 local h = require "helpers"
-local bench = function(name, start_time)
-  local end_time = os.clock()
-  local elapsed_ms = (end_time - start_time) * 1000
-  h.dev.log { string.format("%s: %.3f", name, elapsed_ms), }
-end
-
 local servername = arg[1]
 local query = arg[2] or ""
 query = query:gsub("%s+", "") -- fzy doesn't ignore spaces
@@ -21,41 +13,27 @@ local CHANGED_BUF_BOOST = 20
 local CURR_BUF_BOOST = -1000
 
 local chan = vim.fn.sockconnect("pipe", servername, { rpc = true, })
-
-local cwd_rpc_time = os.clock()
 --- @type string
 local cwd = vim.rpcrequest(chan, "nvim_call_function", "getcwd", {})
-bench("cwd rpc", cwd_rpc_time)
-
 
 --- @class BufInfo
 --- @field name string
 --- @field loaded number
 --- @field changed boolean
 
-local open_buffers_rpc_time = os.clock()
 --- @type BufInfo[]
 local open_buffers = vim.rpcrequest(chan, "nvim_call_function", "getbufinfo", { { buflisted = h.vimscript_true, }, })
-bench("open buffers rpc", open_buffers_rpc_time)
 
-local curr_buf_rpc_time = os.clock()
 --- current buff is the fzf terminal buffer, so the alternate file is the real "alternate" buf
 --- @type string
 local curr_buf = vim.rpcrequest(chan, "nvim_call_function", "expand", { "#:p", })
-bench("curr buf rpc", curr_buf_rpc_time)
 
 vim.fn.chanclose(chan)
 
-local req_time = os.clock()
+local now = os.time()
 local mini_icons = require "mini.icons"
-local fzy = require "fzy-lua-native"
 local frecency_helpers = require "fzf-lua-frecency.helpers"
 local sorted_files_path = frecency_helpers.get_sorted_files_path()
-local frecency_fs = require "fzf-lua-frecency.fs"
-local frecency_algo = require "fzf-lua-frecency.algo"
-bench("req_time", req_time)
-
-local now = os.time()
 
 --- @class FormatFilenameOpts
 --- @field abs_file string
@@ -96,12 +74,10 @@ end
 local scored_files = {}
 
 local dated_files_path = frecency_helpers.get_dated_files_path()
-local read_dated_files_time = os.clock()
+local frecency_fs = require "fzf-lua-frecency.fs"
 local dated_files = frecency_fs.read(dated_files_path)
-bench("read_dated_files_time", read_dated_files_time)
 
 local fd_cmd = "fd --absolute-path --hidden --type f --exclude node_modules --exclude .git --exclude dist"
-local fd_time = os.clock()
 local fd_handle = io.popen(fd_cmd)
 if fd_handle then
   for fd_file in fd_handle:lines() do
@@ -109,10 +85,7 @@ if fd_handle then
   end
   fd_handle:close()
 end
-bench("fd_time", fd_time)
 
-
-local buf_loop_time = os.clock()
 for _, buf in ipairs(open_buffers) do
   if not vim.startswith(buf.name, cwd) then goto continue end
   if buf.loaded == h.vimscript_false then goto continue end
@@ -129,9 +102,8 @@ for _, buf in ipairs(open_buffers) do
 
   ::continue::
 end
-bench("buf_loop_time", buf_loop_time)
 
-local frecency_loop_time = os.clock()
+local frecency_algo = require "fzf-lua-frecency.algo"
 for frecency_file in io.lines(sorted_files_path) do
   if vim.fn.filereadable(frecency_file) == 0 then goto continue end
   if not vim.startswith(frecency_file, cwd) then goto continue end
@@ -147,9 +119,8 @@ for frecency_file in io.lines(sorted_files_path) do
 
   ::continue::
 end
-bench("frecency_loop_time", frecency_loop_time)
 
-local weighted_loop_time = os.clock()
+local fzy = require "fzy-lua-native"
 local weighted_files = {}
 for abs_file, frecency_score in pairs(scored_files) do
   local rel_file = vim.fs.relpath(cwd, abs_file)
@@ -164,15 +135,11 @@ for abs_file, frecency_score in pairs(scored_files) do
   local score = fzy_score * 3 + frecency_score
   table.insert(weighted_files, { file = abs_file, score = score, pos = fzy_pos, })
 end
-bench("weighted_loop_time", weighted_loop_time)
 
-local sort_time = os.clock()
 table.sort(weighted_files, function(a, b)
   return a.score > b.score
 end)
-bench("sort_time", sort_time)
 
-local print_loop_time = os.clock()
 for _, weighted_entry in pairs(weighted_files) do
   local formatted = format_filename {
     abs_file = weighted_entry.file,
@@ -181,5 +148,3 @@ for _, weighted_entry in pairs(weighted_files) do
   }
   h.print_with_flush(formatted)
 end
-bench("print_loop_time", print_loop_time)
-bench("entire_file_time", entire_file_time)
