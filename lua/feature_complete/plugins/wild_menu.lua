351647cfb5a7430e68f20eb9d75b038b0961031b
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-09-03 11:43:08 -0400

    wilder polish

--- lua/feature_complete/plugins/wild_menu.lua
+++ lua/feature_complete/plugins/wild_menu.lua
@@ -7,20 +7,9 @@ vim.keymap.set("c", "<s-tab>", "<nop>")
 -- https://github.com/gelguy/wilder.nvim#neovim-lua-only-config
 wilder.setup { modes = { ":", }, next_key = "<C-n>", previous_key = "<C-p>", }
 
-wilder.set_option("pipeline", {
-  wilder.branch(
-    wilder.cmdline_pipeline { fuzzy = 1, },
-    wilder.vim_search_pipeline()
-  ),
-})
-wilder.set_option("renderer", wilder.wildmenu_renderer
-  {
-    highlighter = wilder.lua_fzy_highlighter(),
-    highlights = {
-      accent = "WilderAccent",
-      selected = "WildMenu",
-    },
-    left = { " ", wilder.wildmenu_spinner(), " ", },
-    right = { " ", wilder.wildmenu_index(), },
-  }
+wilder.set_option("pipeline", { wilder.branch(wilder.cmdline_pipeline { fuzzy = 1, }), })
+wilder.set_option("renderer", wilder.wildmenu_renderer {
+  highlighter = wilder.lua_fzy_highlighter(),
+  highlights = { accent = "WilderAccent", selected = "WildMenu", },
+}
 )
