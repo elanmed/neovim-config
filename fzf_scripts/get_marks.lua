6017e86695180a6fb7085b40584b85c9ac3c6842
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-09-06 14:24:38 -0400

    cleanup getmarks fzf script

--- fzf_scripts/get_marks.lua
+++ fzf_scripts/get_marks.lua
@@ -12,12 +12,10 @@ local mark_list = vim.rpcrequest(chan, "nvim_call_function", "getmarklist", {})
 vim.fn.chanclose(chan)
 if mark_list == nil then return end
 
-local global_marks = ("abcdefghijklmnopqrstuvwxyz"):upper()
-
 local num_marks = 0
 for _, mark_entry in pairs(mark_list) do
   local name = mark_entry.mark:sub(2, 2)
-  if not global_marks:find(name) then goto continue end
+  if not name:match "[A-Z]" then goto continue end
   io.write(("%s|%s"):format(name, mark_entry.file) .. "\n")
   num_marks = num_marks + 1
   ::continue::
