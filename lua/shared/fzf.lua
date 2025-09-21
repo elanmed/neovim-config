f071200e5ed7e83af0cfb2151d79ea81938717d4
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-09-20 20:18:38 -0400

    add back line number to slash search

--- lua/shared/fzf.lua
+++ lua/shared/fzf.lua
@@ -327,7 +327,6 @@ vim.keymap.set("n", "/", function()
   local slash_opts = {
     [[--ghost='/']],
     [[--delimiter='|']],
-    [[--with-nth='2']],
     "--bind", ("'change:execute(echo {q} > %s)'"):format(query_temp),
   }
 
@@ -443,28 +442,5 @@ local function fzf_ui_select(items, opts, on_choice)
 end
 
 vim.ui.select = fzf_ui_select
-
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function(args)
-    local bufnr = args.buf
-    if vim.api.nvim_get_option_value("buftype", { buf = bufnr, }) ~= "" then
-      return
-    end
-
-    local line_count = vim.api.nvim_buf_line_count(bufnr)
-    local buf_name = vim.api.nvim_buf_get_name(bufnr)
-    local size = vim.fn.getfsize(buf_name)
-    local max_size = 1.5 * 1024 * 1024 -- 1.5MB
-
-    if line_count > 5000 or size > max_size then
-      h.notify.doing "bigfile detected"
-      vim.keymap.set("n", "/", function()
-        vim.api.nvim_feedkeys("/", "n", false)
-      end, { buffer = bufnr, })
-      vim.cmd "NoMatchParen"
-      vim.cmd "NeoscrollEnableBufferPM"
-    end
-  end,
-})
 
 return M
