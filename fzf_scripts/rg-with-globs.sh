6c8bf8c70ccb9fe3c9026eb9b0d677041698d092
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-09-28 19:13:41 -0400

    cleanup rg with globs script

--- fzf_scripts/rg-with-globs.sh
+++ fzf_scripts/rg-with-globs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 q="$1"
 echo "$q" >"$(dirname "$0")/prev-rg-query.txt"
-eval "rg --field-match-separator='|' --column --hidden --color=never -g '!.git/**/*' $(nvim --headless -c "lua io.write(require('rg-glob-builder').build('$q'))" +q)"
+flags=$(nvim --headless -c "lua io.write(require('rg-glob-builder').build('$q'))" +q)
+eval "rg --field-match-separator='|' --column --hidden --color=never -g '!.git/**/*' $flags"
