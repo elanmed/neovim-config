f74f37bd0606edba97e75f98f1fdf6ddc2a7d071
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-10-03 22:25:00 -0400

    remove comment

--- lua/feature_complete/plugins/cmp.lua
+++ lua/feature_complete/plugins/cmp.lua
@@ -13,27 +13,3 @@ mini_cmp.setup {
     force_fallback = "<C-x><C-n>",
   },
 }
-
--- vim.api.nvim_create_autocmd("LspAttach", {
---   desc = "Enable inlay hints",
---   callback = function(args)
---     local client = vim.lsp.get_client_by_id(args.data.client_id)
---     if not client then return end
---     local methods = vim.lsp.protocol.Methods
---     if client:supports_method(methods.textDocument_completion) then
---       vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true, })
---
---       vim.keymap.set("i", "<C-x>", function()
---         if next(vim.lsp.get_clients { bufnr = 0, }) then
---           vim.lsp.completion.get()
---         else
---           if vim.bo.omnifunc == "" then
---             vim.cmd [[ call feedkeys("\<C-x>\<C-n>", 'n') ]]
---           else
---             vim.cmd [[ call feedkeys("\<C-x>\<C-o>", 'n') ]]
---           end
---         end
---       end)
---     end
---   end,
--- })
