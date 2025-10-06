aa1f3434a58a046a9a2d55d0411e9938af7b22ba
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-10-06 01:27:11 -0400

    display rel files in global marks fzf

--- fzf_scripts/get_marks.lua
+++ fzf_scripts/get_marks.lua
@@ -9,14 +9,20 @@ local chan = vim.fn.sockconnect("pipe", servername, { rpc = true, })
 
 --- @type MarkListEntry[] | nil
 local mark_list = vim.rpcrequest(chan, "nvim_call_function", "getmarklist", {})
+
+--- @type string | nil
+local cwd = vim.rpcrequest(chan, "nvim_call_function", "getcwd", {})
+
 vim.fn.chanclose(chan)
+if cwd == nil then return end
 if mark_list == nil then return end
 
 local num_marks = 0
 for _, mark_entry in pairs(mark_list) do
   local name = mark_entry.mark:sub(2, 2)
   if not name:match "[A-Z]" then goto continue end
-  io.write(("%s|%s"):format(name, mark_entry.file) .. "\n")
+  local rel_file = vim.fs.relpath(cwd, mark_entry.file)
+  io.write(("%s|%s"):format(name, rel_file) .. "\n")
   num_marks = num_marks + 1
   ::continue::
 end
