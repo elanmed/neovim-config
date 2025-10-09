6b5264e8d93f08b1f405cb677416ca5d259ca09f
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-10-08 23:06:23 -0400

    handle empty alt buf

--- lua/shared/tabline.lua
+++ lua/shared/tabline.lua
@@ -9,7 +9,11 @@ local get_tab_section = function()
 end
 
 local get_buf_section = function()
-  local alt_bufname = vim.api.nvim_buf_get_name(vim.fn.bufnr "#")
+  local alt_bufnr = vim.fn.bufnr "#"
+  if not vim.api.nvim_buf_is_valid(alt_bufnr) then
+    return "#"
+  end
+  local alt_bufname = vim.api.nvim_buf_get_name(alt_bufnr)
   local dirname = vim.fs.basename(vim.fs.dirname(alt_bufname))
   local basename = vim.fs.basename(alt_bufname)
   return "#" .. vim.fs.joinpath(dirname, basename)
