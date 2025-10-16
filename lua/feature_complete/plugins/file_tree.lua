91664ccc48caf247e19783592711712415e56fc7
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-10-16 12:26:14 -0400

    remove mini files setup

--- lua/feature_complete/plugins/file_tree.lua
+++ lua/feature_complete/plugins/file_tree.lua
@@ -1,29 +1,3 @@
-local mini_files = require "mini.files"
-mini_files.setup {
-  mappings = {
-    close = "<esc>",
-    go_in = "l",
-    go_in_plus = "<cr>",
-    go_out = "h",
-    go_out_plus = "",
-    mark_goto = "",
-    mark_set = "",
-    reset = "q",
-    reveal_cwd = "",
-    synchronize = "<bs>",
-  },
-  options = {
-    permanent_delete = false,
-    use_as_default_explorer = false,
-  },
-}
-
-vim.keymap.set("n", "<leader>t", function()
-  if not mini_files.close() then
-    mini_files.open(vim.api.nvim_buf_get_name(0))
-  end
-end, { desc = "Toggle mini files", })
-
 vim.keymap.set("n", "<C-f>", function()
   require "tree".tree {
     tree_win_opts = {
