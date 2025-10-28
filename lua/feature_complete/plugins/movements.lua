cda1baabd651238b7977de7141b5dabec4dca966
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-10-27 21:19:10 -0400

    update to latest marks config

--- lua/feature_complete/plugins/movements.lua
+++ lua/feature_complete/plugins/movements.lua
@@ -25,11 +25,13 @@ vim.g.marks = {
   highlight_char_set = marks.char_sets.local_marks .. marks.char_sets.global_marks .. "<>^",
 }
 
-vim.keymap.set("n", "]a", function() marks.navigate_local_marks { direction = "next", } end)
-vim.keymap.set("n", "[a", function() marks.navigate_local_marks { direction = "prev", } end)
+vim.keymap.set("n", "]a", function() marks.navigate_buffer_marks { direction = "next", } end)
+vim.keymap.set("n", "[a", function() marks.navigate_buffer_marks { direction = "prev", } end)
 vim.keymap.set("n", "]r", function() marks.navigate_global_marks { direction = "next", } end)
 vim.keymap.set("n", "[r", function() marks.navigate_global_marks { direction = "prev", } end)
 vim.keymap.set("n", "<leader>ml", marks.toggle_next_local_mark)
 vim.keymap.set("n", "<leader>mg", marks.toggle_next_global_mark)
 vim.keymap.set("n", "<leader>me", marks.refresh_signs)
 vim.keymap.set("n", "<leader>md", marks.delete_buffer_marks)
+vim.keymap.set("n", "<leader>mq", marks.buffer_marks_to_qf_list)
+vim.keymap.set("n", "<leader>mf", marks.global_marks_to_qf_list)
