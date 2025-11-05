aefda0c26d4abf4ccbb7a87d8466806cd3e7531b
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-11-04 19:19:57 -0500

    move plugins around

--- lua/feature_complete/init.lua
+++ lua/feature_complete/init.lua
@@ -1,29 +1,23 @@
 local h = require "helpers"
 
 vim.pack.add {
+  "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/nvim-treesitter/nvim-treesitter",
+
   "https://github.com/mfussenegger/nvim-jdtls",
   "https://github.com/nvim-mini/mini.nvim",
-  -- far
   "https://github.com/MagicDuck/grug-far.nvim",
-  -- file_tree
-  "https://github.com/elanmed/tree.nvim",
-  -- fzf
   "https://github.com/ibhagwan/fzf-lua",
-  "https://github.com/elanmed/rg-glob-builder.nvim",
+
   "https://github.com/elanmed/fzf-lua-frecency.nvim",
-  -- ff
+  "https://github.com/elanmed/tree.nvim",
+  "https://github.com/elanmed/rg-glob-builder.nvim",
   "https://github.com/elanmed/ff.nvim",
-  -- lsp
-  "https://github.com/neovim/nvim-lspconfig",
   "https://github.com/elanmed/lcs-diff.nvim",
-  -- movements
   "https://github.com/elanmed/seek.nvim",
   "https://github.com/elanmed/ft-highlight.nvim",
   "https://github.com/elanmed/marks.nvim",
-  -- quickfix
   "https://github.com/elanmed/quickfix-preview.nvim",
-  -- treesitter
-  "https://github.com/nvim-treesitter/nvim-treesitter",
 }
 vim.cmd.packadd "nvim.undotree"
 h.require_dir "feature_complete/plugins"
