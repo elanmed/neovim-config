45be7049e3d89071b4fc3ba6c351376065b138d3
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-11-08 13:10:43 -0500

    swap to rg-far

--- lua/feature_complete/init.lua
+++ lua/feature_complete/init.lua
@@ -6,10 +6,10 @@ vim.pack.add {
 
   "https://github.com/mfussenegger/nvim-jdtls",
   "https://github.com/nvim-mini/mini.nvim",
-  "https://github.com/MagicDuck/grug-far.nvim",
   "https://github.com/ibhagwan/fzf-lua",
 
   "https://github.com/elanmed/fzf-lua-frecency.nvim",
+  "https://github.com/elanmed/rg-far.nvim",
   "https://github.com/elanmed/tree.nvim",
   "https://github.com/elanmed/rg-glob-builder.nvim",
   "https://github.com/elanmed/ff.nvim",
@@ -21,3 +21,5 @@ vim.pack.add {
 }
 vim.cmd.packadd "nvim.undotree"
 h.require_dir "feature_complete/plugins"
+
+vim.keymap.set("n", "<leader>r", require "rg-far".open, { desc = "Open the rg-far ui", })
