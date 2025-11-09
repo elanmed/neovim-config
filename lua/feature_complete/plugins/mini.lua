50cb01948ad4a23791b6b92d447ce5c406fb674b
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-11-08 21:06:58 -0500

    remove fixme hi

--- lua/feature_complete/plugins/mini.lua
+++ lua/feature_complete/plugins/mini.lua
@@ -22,7 +22,6 @@ local hipatterns = require "mini.hipatterns"
 -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-hipatterns.md#example-usage
 hipatterns.setup {
   highlighters = {
-    fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme", },
     todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo", },
     hex_color = hipatterns.gen_highlighter.hex_color(),
   },
