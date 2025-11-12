be5c2e2f24061b1ceca940f77d6d16ecdfc5dc8e
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-11-11 20:24:35 -0500

    auto install treesitter parsers

--- lua/feature_complete/plugins/treesitter.lua
+++ lua/feature_complete/plugins/treesitter.lua
@@ -1,22 +1,38 @@
-require "nvim-treesitter.configs".setup {
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-  folding = {
-    enable = false,
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "<leader>v",
-      node_incremental = "an",
-      scope_incremental = "an",
-      node_decremental = "in",
-    },
-  },
-}
+local h = require "helpers"
 
 vim.filetype.add { extension = { mdx = "mdx", }, }
 vim.treesitter.language.register("markdown", "mdx")
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function() pcall(vim.treesitter.start) end,
+})
+
+local treesitter = require "nvim-treesitter"
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(event)
+    local filetype = event.match
+    local lang = vim.treesitter.language.get_lang(filetype)
+    if lang == nil then return end
+
+    local is_installed = vim.treesitter.language.add(lang)
+
+    if not is_installed then
+      local available_langs = treesitter.get_available()
+
+      if vim.tbl_contains(available_langs, lang) then
+        treesitter.install { lang, }:wait(30 * 1000)
+      end
+    end
+
+    pcall(vim.treesitter.start, event.buf, lang)
+  end,
+})
+
+vim.api.nvim_create_autocmd("PackChanged", {
+  pattern = "nvim-treesitter",
+  callback = function()
+    h.notify.doing "Updating treesitter parsers"
+    treesitter.update(nil, { summary = true, })
+  end,
+})
