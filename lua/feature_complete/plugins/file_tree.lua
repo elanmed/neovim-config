d2e0f82c17f35058d7f5b1f4e377d244f44ef51e
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2025-11-29 23:48:46 -0500

    update to latest tree config

--- lua/feature_complete/plugins/file_tree.lua
+++ lua/feature_complete/plugins/file_tree.lua
@@ -22,11 +22,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "l", "<Plug>TreeInDir", { buffer = args.buf, })
     vim.keymap.set("n", "yr", "<Plug>TreeYankRelativePath", { buffer = args.buf, })
     vim.keymap.set("n", "ya", "<Plug>TreeYankAbsolutePath", { buffer = args.buf, })
+    vim.keymap.set("n", "yd", "<Plug>TreeYankDirectoryPath", { buffer = args.buf, })
     vim.keymap.set("n", "o", "<Plug>TreeCreate", { buffer = args.buf, })
     vim.keymap.set("n", "e", "<Plug>TreeRefresh", { buffer = args.buf, })
     vim.keymap.set("n", "r", "<Plug>TreeRename", { buffer = args.buf, })
     vim.keymap.set("n", "dd", "<Plug>TreeDelete", { buffer = args.buf, })
+    vim.keymap.set("n", "yy", "<Plug>TreeCopy", { buffer = args.buf, })
 
     vim.keymap.set("v", "d", "<Plug>TreeDelete", { buffer = args.buf, })
+    vim.keymap.set("v", "yy", "<Plug>TreeCopy", { buffer = args.buf, })
   end,
 })
