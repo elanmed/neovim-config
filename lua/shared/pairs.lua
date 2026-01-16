ec29222451e77699214c76936b6461ef10b65f73
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-01-16 00:30:06 -0500

    add backtick pair

--- lua/shared/pairs.lua
+++ lua/shared/pairs.lua
@@ -13,6 +13,7 @@ vim.keymap.set("i", ";c", "{}<left>")
 vim.keymap.set("i", ";a", "<><left>")
 vim.keymap.set("i", ";d", [[""<left>]])
 vim.keymap.set("i", ";s", [[''<left>]])
+vim.keymap.set("i", ";t", [[``<left>]])
 
 vim.keymap.set("i", "<C-d>", function()
   local char_idx_0i = vim.api.nvim_win_get_cursor(0)[2]
