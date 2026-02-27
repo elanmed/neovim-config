1959bed6346844b0798818b931b9535dc904baaf
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-02-26 23:11:26 -0500

    add basic change surround

--- lua/shared/surround.lua
+++ lua/shared/surround.lua
@@ -34,19 +34,40 @@ end
 
 vim.keymap.set("n", "ds", function()
   local char = vim.fn.nr2char(vim.fn.getchar())
-  local open, close = get_pair(char)
-  local pair_pos = find_surrounding_pair_0i(open, close)
+  local pair_pos = find_surrounding_pair_0i(get_pair(char))
   if pair_pos == nil then
     require "helpers".notify.error "No matching pair"
     return
   end
 
   vim.api.nvim_buf_set_text(0,
-    pair_pos.close_line, pair_pos.close_col, pair_pos.close_line, pair_pos.close_col + 1,
+    pair_pos.close_row, pair_pos.close_col, pair_pos.close_row, pair_pos.close_col + 1,
     { "", }
   )
   vim.api.nvim_buf_set_text(0,
     pair_pos.open_row, pair_pos.open_col, pair_pos.open_row, pair_pos.open_col + 1,
     { "", }
+  )
+end)
+
+vim.keymap.set("n", "cs", function()
+  local old_char = vim.fn.nr2char(vim.fn.getchar())
+  local new_char = vim.fn.nr2char(vim.fn.getchar())
+
+  local old_pair_pos = find_surrounding_pair_0i(get_pair(old_char))
+  if old_pair_pos == nil then
+    require "helpers".notify.error "No matching pair"
+    return
+  end
+
+  local new_open, new_close = get_pair(new_char)
+
+  vim.api.nvim_buf_set_text(0,
+    old_pair_pos.close_row, old_pair_pos.close_col, old_pair_pos.close_row, old_pair_pos.close_col + 1,
+    { new_close, }
+  )
+  vim.api.nvim_buf_set_text(0,
+    old_pair_pos.open_row, old_pair_pos.open_col, old_pair_pos.open_row, old_pair_pos.open_col + 1,
+    { new_open, }
   )
 end)
