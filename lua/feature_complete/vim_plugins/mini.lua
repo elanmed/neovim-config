53340da406fc56904d70d882aa8d0d990c58e6e1
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-03-05 11:48:09 -0500

    remove mini.keymap

--- lua/feature_complete/vim_plugins/mini.lua
+++ lua/feature_complete/vim_plugins/mini.lua
@@ -14,18 +14,34 @@ h.utils.lazy_setup(function()
   }
 end)
 
-h.utils.lazy_setup(function()
-  local input_wasted_keys = function(key)
-    local action = function()
-      if vim.bo.buftype ~= "" then return end
-      vim.fn.input "Wasted keys: "
+local function setup_wasted_key_detection(key, threshold)
+  local press_count = 0
+  local timer = nil
+
+  vim.keymap.set("n", key, function()
+    if vim.bo.buftype ~= "" then
+      press_count = 0
+      return vim.api.nvim_feedkeys(key, "n", false)
     end
-    require "mini.keymap".map_combo("n", string.rep(key, 5), action)
-  end
-  input_wasted_keys "h"
-  input_wasted_keys "j"
-  input_wasted_keys "k"
-  input_wasted_keys "l"
-  input_wasted_keys "w"
-  input_wasted_keys "b"
-end)
+
+    press_count = press_count + 1
+    if timer then vim.fn.timer_stop(timer) end
+
+    if press_count >= threshold then
+      press_count = 0
+      vim.fn.input "Wasted keys: "
+      return
+    end
+
+    timer = vim.fn.timer_start(200, function()
+      press_count = 0
+    end)
+
+    vim.api.nvim_feedkeys(key, "n", false)
+  end)
+end
+
+local wasted_keys = { "h", "j", "k", "l", "w", "b", }
+for _, key in ipairs(wasted_keys) do
+  setup_wasted_key_detection(key, 5)
+end
