0d2bf96d930c0ca868783a293f0b6605614149ea
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-03-06 10:57:36 -0500

    move alt to status line

--- lua/shared/tabline.lua
+++ lua/shared/tabline.lua
@@ -1,5 +1,19 @@
 vim.o.showtabline = 2
+vim.o.laststatus = 2
 vim.o.tabline = "%!v:lua.Tabline()"
+vim.o.statusline = "%!v:lua.Statusline()"
+
+--- @param bufnr number
+local get_name = function(bufnr)
+  if vim.bo[bufnr].buftype == "terminal" then return "[terminal]" end
+
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  if bufname == "" then return "[unnamed]" end
+
+  local dirname = vim.fs.basename(vim.fs.dirname(bufname))
+  local basename = vim.fs.basename(bufname)
+  return vim.fs.joinpath(dirname, basename)
+end
 
 local get_tab_section = function()
   local tabs = vim.api.nvim_list_tabpages()
@@ -8,30 +22,23 @@ local get_tab_section = function()
   return table.concat({ "%#Search#", curr_tab, "%#TabLine#", }, " ")
 end
 
-local get_buf_section = function()
-  local bufnr = vim.fn.bufnr "#"
+--- @param buf_symbol "%" | "#"
+local get_buf_section = function(buf_symbol)
+  local bufnr = vim.fn.bufnr(buf_symbol)
   if not vim.api.nvim_buf_is_valid(bufnr) then return "" end
 
-  local name = (function()
-    if vim.bo[bufnr].buftype == "terminal" then return "[terminal]" end
-
-    local bufname = vim.api.nvim_buf_get_name(bufnr)
-    if bufname == "" then return "[unnamed]" end
-
-    local dirname = vim.fs.basename(vim.fs.dirname(bufname))
-    local basename = vim.fs.basename(bufname)
-    return vim.fs.joinpath(dirname, basename)
-  end)()
-  return "%#TabLineTitle#ALT %#TabLineSel#" .. name
+  if buf_symbol == "%" then
+    return "%#TabLineTitle#EDIT: %#TabLineSel#" .. get_name(bufnr)
+  elseif buf_symbol == "#" then
+    return "ALT: " .. get_name(bufnr)
+  end
 end
 
 _G.Tabline = function()
-  local alt_section = (function()
-    if vim.fn.bufnr "#" == vim.fn.bufnr "%" then
-      return ""
-    end
-    return get_buf_section()
-  end)()
+  return table.concat({ get_tab_section(), get_buf_section "%", }, " ")
+end
 
-  return table.concat({ get_tab_section(), alt_section, }, " ")
+_G.Statusline = function()
+  if vim.fn.bufnr "#" == vim.fn.bufnr "%" then return "" end
+  return get_buf_section "#"
 end
