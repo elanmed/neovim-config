6e2f47bac0ad064ef6f3e3d8ef4a469e6669dac6
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-03-06 09:32:15 -0500

    move wasted_key_detection to remaps

--- lua/feature_complete/vim_plugins/mini.lua
+++ lua/feature_complete/vim_plugins/mini.lua
@@ -14,34 +14,3 @@ h.utils.lazy_setup(function()
   }
 end)
 
-local function setup_wasted_key_detection(key, threshold)
-  local press_count = 0
-  local timer = nil
-
-  vim.keymap.set("n", key, function()
-    if vim.bo.buftype ~= "" then
-      press_count = 0
-      return vim.api.nvim_feedkeys(vim.v.count .. key, "n", false)
-    end
-
-    press_count = press_count + 1
-    if timer then vim.fn.timer_stop(timer) end
-
-    if press_count >= threshold then
-      press_count = 0
-      vim.fn.input "Wasted keys: "
-      return
-    end
-
-    timer = vim.fn.timer_start(200, function()
-      press_count = 0
-    end)
-
-    vim.api.nvim_feedkeys(vim.v.count .. key, "n", false)
-  end)
-end
-
-local wasted_keys = { "h", "j", "k", "l", "w", "b", }
-for _, key in ipairs(wasted_keys) do
-  setup_wasted_key_detection(key, 5)
-end
--- lua/shared/remaps.lua
+++ lua/shared/remaps.lua
@@ -207,6 +207,39 @@ vim.keymap.set("n", "<leader>mD", function()
   pcall(require "marks".refresh_signs)
   require "helpers".notify.doing "Deleted all marks"
 end)
+
+local function setup_wasted_key_detection(key, threshold)
+  local press_count = 0
+  local timer = nil
+
+  vim.keymap.set("n", key, function()
+    if vim.bo.buftype ~= "" then
+      press_count = 0
+      return vim.api.nvim_feedkeys(vim.v.count .. key, "n", false)
+    end
+
+    press_count = press_count + 1
+    if timer then vim.fn.timer_stop(timer) end
+
+    if press_count >= threshold then
+      press_count = 0
+      vim.fn.input "Wasted keys: "
+      return
+    end
+
+    timer = vim.fn.timer_start(200, function()
+      press_count = 0
+    end)
+
+    vim.api.nvim_feedkeys(vim.v.count .. key, "n", false)
+  end)
+end
+
+local wasted_keys = { "h", "j", "k", "l", "w", "b", }
+for _, key in ipairs(wasted_keys) do
+  setup_wasted_key_detection(key, 5)
+end
+
 vim.keymap.set({ "i", "c", }, "<C-r><C-;>", "<C-r>+")
 vim.keymap.set({ "i", "c", }, "<C-r>;", "<C-r>+")
 vim.keymap.set({ "i", "c", }, "<C-h>", "<left>")
