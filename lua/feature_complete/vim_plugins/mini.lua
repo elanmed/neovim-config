05c669788239ce4f4acd7e718b85d5339cc45196
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-03-09 11:18:46 -0400

    use mini lsp icons

--- lua/feature_complete/vim_plugins/mini.lua
+++ lua/feature_complete/vim_plugins/mini.lua
@@ -1,3 +1,7 @@
 local h = require "helpers"
-h.utils.lazy_setup(function() require "mini.icons".setup() end)
+h.utils.lazy_setup(function()
+  local icons = require "mini.icons"
+  icons.setup()
+  icons.tweak_lsp_kind()
+end)
 h.utils.lazy_setup(function() require "mini.splitjoin".setup() end)
