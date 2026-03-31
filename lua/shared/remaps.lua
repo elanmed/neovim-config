df0196bf70f976288ea8f3b099fb29025817e401
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-03-30 22:46:25 -0400

    add back <leader>d

--- lua/shared/remaps.lua
+++ lua/shared/remaps.lua
@@ -12,9 +12,7 @@ vim.keymap.set("n", "<leader>/s", ":%s/\\<\\>\\C/<left><left><left><left><left>"
   { desc = "%s in the current buffer, case and word sensitive", })
 vim.keymap.set("n", "<leader>n", vim.cmd.nohlsearch, { desc = "Turn off highlighting", })
 vim.keymap.set("n", "<leader>x", vim.cmd.tabclose, { desc = "Close the current tab", })
-
--- vim.keymap.set("n", "<leader>d", "<nop>", { desc = "Close the current buffer", })
-
+vim.keymap.set("n", "<leader>d", "<nop>", { desc = "Close the current buffer", })
 vim.keymap.set("n", "<leader>;", ":", { desc = ":", })
 vim.keymap.set("n", "x", [["_x]], { desc = "x to the black hole buffer", })
 vim.keymap.set("n", "X", [["_X]], { desc = "X to the black hole buffer", })
