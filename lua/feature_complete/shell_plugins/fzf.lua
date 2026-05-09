1be76c16664589cba5de48c85115737293f233b1
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-05-08 23:34:22 -0400

    update: <leader>o flags, add <leader>zo with exclude flags

--- lua/feature_complete/shell_plugins/fzf.lua
+++ lua/feature_complete/shell_plugins/fzf.lua
@@ -434,17 +434,39 @@ vim.keymap.set("n", "<leader>zr", function()
   fzf { is_replay = true, }
 end, { desc = "fzf replay", })
 
+local exact_search_flags = table.concat({ "-w", "-s", "-F", }, " ")
+local exclude_flags = table.concat({
+  "-g",
+  "'!*test*'",
+  "-g",
+  "'!*spec*'",
+  "-g",
+  "'!*.json'",
+}, " ")
+
 vim.keymap.set("n", "<leader>o", function()
   local cword = vim.fn.expand "<cword>"
-  rg(([[-g '!*test*' -g '!*spec*' -g '!*.json' -F '%s' ]]):format(cword))
-end, { desc = "fzf rg excluding common files", })
+  rg(([[%s '%s']]):format(exact_search_flags, cword))
+end, { desc = "fzf rg with exact search flags", })
+
+vim.keymap.set("n", "<leader>zo", function()
+  local cword = vim.fn.expand "<cword>"
+  rg(([[%s %s '%s']]):format(exact_search_flags, exclude_flags, cword))
+end, { desc = "fzf rg with exact search, exclude flags", })
 
 vim.keymap.set("v", "<leader>o", function()
   local region = vim.fn.getregion(vim.fn.getpos "v", vim.fn.getpos ".")
   if #region > 0 then
-    rg(([[-g '!*test*' -g '!*spec*' -g '!*.json' -F '%s' ]]):format(region[1]))
+    rg(([[%s '%s']]):format(exact_search_flags, region[1]))
   end
-end, { desc = "fzf rg excluding common files", })
+end, { desc = "fzf rg with exact search flags", })
+
+vim.keymap.set("v", "<leader>zo", function()
+  local region = vim.fn.getregion(vim.fn.getpos "v", vim.fn.getpos ".")
+  if #region > 0 then
+    rg(([[%s %s '%s']]):format(exact_search_flags, exclude_flags, region[1]))
+  end
+end, { desc = "fzf rg with exact search, exclude flags", })
 
 
 local function get_stripped_filename()
