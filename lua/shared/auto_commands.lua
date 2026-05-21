e0fb0df0e6118f89c45b3bb42493c09b394471fe
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-05-21 00:29:41 -0400

    fix: swap to vim.hl.hl_op

--- lua/shared/auto_commands.lua
+++ lua/shared/auto_commands.lua
@@ -34,7 +34,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true, }),
-  callback = function() vim.highlight.on_yank() end,
+  callback = function() vim.hl.hi_op() end,
 })
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
