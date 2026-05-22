4c87df6e3e85cf4b719734e9781d7743411b11e4
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-05-21 20:55:59 -0400

    fix: typo

--- lua/shared/auto_commands.lua
+++ lua/shared/auto_commands.lua
@@ -34,7 +34,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("HighlightOnYank", { clear = true, }),
-  callback = function() vim.hl.hi_op() end,
+  callback = function() vim.hl.hl_op() end,
 })
 
 vim.api.nvim_create_autocmd("BufWinEnter", {
