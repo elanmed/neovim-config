26aafc5ab25c74d2c3b6823b0f5031b4dd0a2a60
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-06-15 00:55:24 -0400

    refactor: remove unecessary arg

--- lua/feature_complete/shell_plugins/fzf.lua
+++ lua/feature_complete/shell_plugins/fzf.lua
@@ -307,7 +307,7 @@ vim.keymap.set("n", "<leader>i", function()
     sinklist = build_sinklist {
       get_filename = function(entry) return entry end,
       get_qf_entry = function(entry) return { lnum = 1, col = 0, filename = entry, } end,
-      after_edit = function(entry)
+      after_edit = function()
         vim.api.nvim_win_set_cursor(0, { 1, 0, })
         vim.cmd [[execute "normal \<Plug>GitDiffNextHunk"]]
       end,
