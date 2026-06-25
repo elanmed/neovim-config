9b46db211b3d2718d4ad364254ab0728a9756afd
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-06-24 23:35:29 -0400

    refactor: add regex named vars

--- fzf_scripts/get_git_hunks.lua
+++ fzf_scripts/get_git_hunks.lua
@@ -1,13 +1,26 @@
+local start = "^"
+local capture_rest = "(.+)"
+local skip_digits = "%d+"
+local optional_comma = ",?"
+local optional_digits = "%d*"
+local optional_count = "(" .. optional_comma .. optional_digits .. ")"
+local capture_digits = "(%d+)"
+local any_remaining = "(.*)"
+
+local file_header_pattern = start .. "%+%+%+ b/" .. capture_rest
+local hunk_header_pattern = start ..
+    "@@ %-" .. skip_digits .. optional_count .. " %+" .. capture_digits .. optional_count .. " @@ ?" .. any_remaining
+
 local function get_tracked_hunks()
   local diff_output = vim.fn.systemlist "git diff HEAD --unified=0"
   local current_file = nil
   local hunks = {}
   for _, line in ipairs(diff_output) do
-    local file = line:match "^%+%+%+ b/(.+)"
+    local file = line:match(file_header_pattern)
     if file ~= nil then
       current_file = file
     end
-    local lnum, context = line:match "^@@ %-%d+,?%d* %+(%d+),?%d* @@ ?(.*)"
+    local lnum, context = line:match(hunk_header_pattern)
     if lnum ~= nil and current_file ~= nil then
       table.insert(hunks, ("%s|%s|1|%s"):format(current_file, lnum, context))
     end
