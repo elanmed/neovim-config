be0dcebbd6c58c6dac8f130c7c9aee52446e4007
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2026-06-24 23:30:03 -0400

    refactor: get_git_hunks

--- fzf_scripts/get_git_hunks.lua
+++ fzf_scripts/get_git_hunks.lua
@@ -1,19 +1,26 @@
-local tracked_output = vim.fn.systemlist "git diff HEAD --unified=0"
-
-local current_file = nil
-for _, line in ipairs(tracked_output) do
-  local file = line:match "^%+%+%+ b/(.+)"
-  if file then
-    current_file = file
+local function get_tracked_hunks()
+  local diff_output = vim.fn.systemlist "git diff HEAD --unified=0"
+  local current_file = nil
+  local hunks = {}
+  for _, line in ipairs(diff_output) do
+    local file = line:match "^%+%+%+ b/(.+)"
+    if file ~= nil then
+      current_file = file
+    end
+    local lnum, context = line:match "^@@ %-%d+,?%d* %+(%d+),?%d* @@ ?(.*)"
+    if lnum ~= nil and current_file ~= nil then
+      table.insert(hunks, ("%s|%s|1|%s"):format(current_file, lnum, context))
+    end
   end
-
-  local lnum, context = line:match "^@@ %-%d+,?%d* %+(%d+),?%d* @@ ?(.*)"
-  if lnum and current_file then
-    io.write(("%s|%s|1|%s\n"):format(current_file, lnum, context))
-  end
+  return hunks
 end
 
-local untracked_files = vim.fn.systemlist "git ls-files --others --exclude-standard"
-for _, untracked_file in ipairs(untracked_files) do
-  io.write(("%s|1|1|untracked\n"):format(untracked_file))
+local function get_untracked_entries()
+  return vim.tbl_map(function(untracked_file)
+    return ("%s|1|1|untracked"):format(untracked_file)
+  end, vim.fn.systemlist "git ls-files --others --exclude-standard")
+end
+
+for _, entry in ipairs(vim.list_extend(get_tracked_hunks(), get_untracked_entries())) do
+  io.write(entry .. "\n")
 end
